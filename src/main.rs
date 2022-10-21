2bf1d92cfab0447fb52fad8f93bccdf18fa69f8b
etash <etashdoteth@gmail.com>
2022-10-20 21:58:27 -0400

    added some tarpc-related setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, Kakarot!");
+#![allow(unused)]
+
+use futures::{
+    future::{self, Ready},
+    prelude::*,
+};
+
+use tarpc::{
+    client, context,
+    server::{self, incoming::Incoming, Channel},
+};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    println!("👋 Hello, Kakarot!");
+    Ok(())
 }
