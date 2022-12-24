1a5e242da05dcb5324cbbb9e9461adec8390c2d6
Flydexo <flydexo@gmail.com>
2022-12-24 18:21:39 +0100

    introduction of hex bytes & hex quantity

--- src/methods/web3/sha_3.rs
+++ src/methods/web3/sha_3.rs
@@ -1,8 +1,6 @@
 use crate::utils::error::default_error_invalid_params;
-use crate::{
-    crypto::sha3::Sha3,
-    utils::hex::{format_hex, parse_hex},
-};
+use crate::utils::hex::parse_hex_bytes;
+use crate::{crypto::sha3::Sha3, utils::hex::format_hex};
 use crypto::digest::Digest;
 use jsonrpc_http_server::jsonrpc_core::{Error, Params, Value};
 
@@ -18,7 +16,7 @@ pub async fn execute(_params: Params) -> Result<Value, Error> {
                 return Err(default_error_invalid_params());
             }
             let mut hasher = Sha3::keccak256(); // image of an hasher https://yt3.ggpht.com/icJZDespcjNLPi-_1qA-_kYIfWq66_mJM-721fhpA1f-yZ6st5-Wooqn0MS9TQXj8jTbYNVpoQ=s176-c-k-c0x00ffffff-no-rj
-            match parse_hex(&data[0]) {
+            match parse_hex_bytes(&data[0]) {
                 Ok(hex) => {
                     hasher.input(&hex);
                     return Ok(Value::String(format_hex(&hasher.result_str())));
--- src/utils/hex.rs
+++ src/utils/hex.rs
@@ -3,12 +3,25 @@ use jsonrpc_http_server::jsonrpc_core::Error;
 
 use crate::utils::error::default_error_invalid_params;
 
-pub fn parse_hex(hex: &String) -> Result<Vec<u8>, Error> {
-    if &hex[0..2] != "0x" {
+pub fn parse_hex_bytes(hex: &String) -> Result<Vec<u8>, Error> {
+    if hex.len() < 3 || &hex[0..2] != "0x" || hex.len() % 2 == 1 {
         return Err(default_error_invalid_params());
     }
     match Vec::from_hex(&hex[2..]) {
-        // add padding 0 if odd length
+        Ok(v) => Ok(v),
+        Err(_) => Err(default_error_invalid_params()),
+    }
+}
+
+pub fn parse_hex_quantity<T: FromHex>(hex: &String) -> Result<T, Error> {
+    if hex.len() < 3 || &hex[0..2] != "0x" {
+        return Err(default_error_invalid_params());
+    }
+    let mut _hex = hex[2..].to_string();
+    if hex.len() % 2 == 1 {
+        _hex = "0".to_string() + &_hex;
+    }
+    match T::from_hex(&_hex) {
         Ok(v) => Ok(v),
         Err(_) => Err(default_error_invalid_params()),
     }
