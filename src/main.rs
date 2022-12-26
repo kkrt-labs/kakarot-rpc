bd38775d98fbf8c70e10df1006aecf93611eda5d
Flydexo <flydexo@gmail.com>
2022-12-26 19:59:32 +0100

    old implementation

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,17 @@
-use clap::Parser;
-use jsonrpc_http_server::{jsonrpc_core::IoHandler, ServerBuilder};
+#![allow(unused)]
 
-extern crate crypto;
+use futures::{
+    future::{self, Ready},
+    prelude::*,
+};
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(short, long)]
-    port: u16,
-}
+use tarpc::{
+    client, context,
+    server::{self, incoming::Incoming, Channel},
+};
 
-fn main() {
-    let args = Args::parse();
-    let io = IoHandler::default();
-
-    let server = ServerBuilder::new(io)
-        .threads(3)
-        .start_http(
-            &("127.0.0.1:".to_string() + &args.port.to_string())
-                .parse()
-                .unwrap(),
-        )
-        .unwrap();
-
-    println!("Kakarot RPC Adapter running on port {} !", args.port);
-
-    server.wait();
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    println!("👋 Hello, Kakarot!");
+    Ok(())
 }
