3da21898316a3570016b7f327a1154acc6fc400c
Elias Tazartes <elias.tazartes@gmail.com>
2023-01-11 15:59:52 +0100

    feat: change rpc skeleton to match reth's rpc architecture

--- rpc/src/main.rs
+++ rpc/src/main.rs
@@ -1,12 +1,3 @@
-use kakarot_rpc::build_rocket_server;
-use rocket::{Build, Rocket};
-#[macro_use]
-extern crate rocket;
-
-#[launch]
-async fn rocket() -> Rocket<Build> {
-    env_logger::init();
-
-    info!("starting Kakarot RPC...");
-    build_rocket_server().await
+fn main() {
+    todo!()
 }
