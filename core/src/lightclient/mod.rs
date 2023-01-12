c32197fe5c3371c81a5dd97736c6695496b7874c
danilowhk <danilowhk@gmail.com>
2023-01-12 17:43:54 -0300

    eth_blockNumber

---
+++ core/src/lightclient/mod.rs
@@ -0,0 +1,38 @@
+use starknet::{
+    providers::jsonrpc::{
+
+        HttpTransport, JsonRpcClient,
+    },
+};
+use eyre::{Result};
+use url::Url;
+
+pub struct StarknetClient {
+    client: JsonRpcClient<HttpTransport>,
+}
+
+impl StarknetClient {
+    pub fn new(starknet_rpc: &String) -> Result<Self>{
+        let url = Url::parse(starknet_rpc.clone().as_str())?;
+        Ok(Self {
+            client: JsonRpcClient::new(HttpTransport::new(url)),
+        })
+    }
+
+    /// Get the number of transactions in a block given a block id.
+    /// The number of transactions in a block.
+    ///
+    /// # Arguments
+    ///
+    ///
+    ///
+    /// # Returns
+    ///
+    ///  * `block_number(u64)` - The block number.
+    /// 
+    /// `Ok(ContractClass)` if the operation was successful.
+    /// `Err(eyre::Report)` if the operation failed.
+    pub async fn block_number(&self) -> Result<u64> {
+        self.client.block_number().await.map_err(|e| eyre::eyre!(e))
+    }
+}
--- rpc/src/main.rs
+++ rpc/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    todo!()
+use eyre::{eyre, Result};
+use futures::future::pending;
+use kakarot_rpc::run_server;
+use tracing_subscriber::util::SubscriberInitExt;
+use kakarot_rpc_core::lightclient::StarknetClient;
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    let filter = tracing_subscriber::EnvFilter::try_from_default_env()?
+    .add_directive("jsonrpsee[method_call{name = \"eth_chainId\"}]=trace".parse()?);
+    tracing_subscriber::FmtSubscriber::builder().with_env_filter(filter).finish().try_init()?;
+
+    let starknet_rpc = std::env::var("STARKNET_RPC_URL")
+    .map_err(|_| eyre!("Missing mandatory environment variable: STARKNET_RPC_URL"))?;
+
+    let starknet_lightclient = StarknetClient::new(&starknet_rpc)?;
+
+    let server_addr = run_server(starknet_lightclient).await?;
+    let url = format!("http://{}", server_addr);
+    
+    println!("{url}");
+
+    pending::<()>().await;
+
+    Ok(())
 }
+
