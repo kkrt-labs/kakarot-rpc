dee9c7609f866ebc438ee3fa369d4608368c0724
Elias Tazartes <66871571+Eikix@users.noreply.github.com>
2023-01-15 11:39:53 +0000

    Docs/eth get block by number (#33)

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -4,18 +4,18 @@ use std::net::SocketAddr;
 mod eth_rpc;
 use eth_rpc::{EthApiServer, KakarotEthRpc};
 use eyre::Result;
-use jsonrpsee::server::ServerBuilder;
+use jsonrpsee::server::{ServerBuilder, ServerHandle};
 use kakarot_rpc_core::lightclient::StarknetClient;
 
-pub async fn run_server(starknet_client: StarknetClient) -> Result<SocketAddr> {
+pub async fn run_server(starknet_client: StarknetClient) -> Result<(SocketAddr, ServerHandle)> {
     let server = ServerBuilder::default()
         .build("127.0.0.1:03030".parse::<SocketAddr>()?)
         .await?;
 
-    let addr = server.local_addr();
+    let addr = server.local_addr()?;
 
-    let rpc_calls = KakarotEthRpc { starknet_client };
+    let rpc_calls = KakarotEthRpc::new(starknet_client);
     let handle = server.start(rpc_calls.into_rpc())?;
-    tokio::spawn(handle.stopped());
-    Ok(addr.unwrap())
+
+    Ok((addr, handle))
 }
