cf6b8690db7e80aa305f1c4ceeec9a65ad7dc649
Elias Tazartes <66871571+Eikix@users.noreply.github.com>
2023-01-19 14:11:47 +0000

    feat: implement getBlockByNumber and getBlockByHash (#37)

--- core/src/lightclient/mod.rs
+++ core/src/lightclient/mod.rs
@@ -1,8 +1,12 @@
 use eyre::Result;
 use jsonrpsee::types::error::CallError;
+use reth_rpc_types::RichBlock;
+use starknet::providers::jsonrpc::models::BlockId as StarknetBlockId;
 use starknet::providers::jsonrpc::{HttpTransport, JsonRpcClient, JsonRpcClientError};
 use thiserror::Error;
 use url::Url;
+
+use crate::helpers::{starknet_block_to_eth_block, MaybePendingStarknetBlock};
 
 #[derive(Error, Debug)]
 pub enum LightClientError {
@@ -51,5 +55,31 @@ impl StarknetClient {
     pub async fn block_number(&self) -> Result<u64, LightClientError> {
         let block_number = self.client.block_number().await?;
         Ok(block_number)
+    }
+
+    /// Get the block given a block id.
+    /// The block.
+    /// # Arguments
+    /// * `block_id(StarknetBlockId)` - The block id.
+    /// * `hydrated_tx(bool)` - Whether to hydrate the transactions.
+    /// # Returns
+    /// `Ok(RichBlock)` if the operation was successful.
+    /// `Err(LightClientError)` if the operation failed.
+    pub async fn get_eth_block_from_starknet_block(
+        &self,
+        block_id: StarknetBlockId,
+        hydrated_tx: bool,
+    ) -> Result<RichBlock, LightClientError> {
+        let starknet_block = if hydrated_tx {
+            MaybePendingStarknetBlock::BlockWithTxs(
+                self.client.get_block_with_txs(&block_id).await?,
+            )
+        } else {
+            MaybePendingStarknetBlock::BlockWithTxHashes(
+                self.client.get_block_with_tx_hashes(&block_id).await?,
+            )
+        };
+        let block = starknet_block_to_eth_block(starknet_block);
+        Ok(block)
     }
 }
