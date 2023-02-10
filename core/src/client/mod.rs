a347fcba9f999438dd3d6e6f448f96422c8c30e5
danilowhk <danilowhk@gmail.com>
2023-02-10 16:37:39 -0300

    change to get_evm_address

--- core/src/client/mod.rs
+++ core/src/client/mod.rs
@@ -823,7 +823,7 @@ impl StarknetClient for StarknetClientImpl {
 
                 ether_tx.nonce = U256::from(l1_handler_tx.nonce);
                 ether_tx.from =
-                    starknet_address_to_ethereum_address(l1_handler_tx.contract_address);
+                    self.get_evm_address(l1_handler_tx.contract_address, StarknetBlockId::Tag(BlockTag::Latest)).await?;
                 // Define gas_price data
                 ether_tx.gas_price = None;
                 // Extracting the data
