6639d8acf201a5313d6350390ebac753a60ad95d
Gregory Edison <gregory.edison1993@gmail.com>
2023-06-23 18:14:04 +0200

    add additional error handling, conversion for felt

--- crates/core/src/models/felt.rs
+++ crates/core/src/models/felt.rs
@@ -1,5 +1,20 @@
-use reth_primitives::{H256, U256};
-use starknet::core::types::FieldElement;
+use reth_primitives::{H160, H256, U256};
+use starknet::core::types::{FieldElement, FromByteArrayError};
+use thiserror::Error;
+
+use crate::client::errors::EthApiError;
+
+#[derive(Debug, Error)]
+pub enum Felt252WrapperError {
+    #[error(transparent)]
+    FromByteArrayError(#[from] FromByteArrayError),
+}
+
+impl From<Felt252WrapperError> for EthApiError {
+    fn from(err: Felt252WrapperError) -> Self {
+        EthApiError::ConversionError(err.into())
+    }
+}
 
 pub struct Felt252Wrapper(FieldElement);
 
@@ -15,6 +30,20 @@ impl From<Felt252Wrapper> for FieldElement {
     }
 }
 
+impl From<u64> for Felt252Wrapper {
+    fn from(u64: u64) -> Self {
+        let felt = FieldElement::from(u64);
+        Self(felt)
+    }
+}
+
+impl From<H160> for Felt252Wrapper {
+    fn from(h160: H160) -> Self {
+        let felt = FieldElement::from_byte_slice_be(&h160.0).unwrap(); // safe unwrap since H160 is 20 bytes
+        Self(felt)
+    }
+}
+
 impl From<Felt252Wrapper> for H256 {
     fn from(felt: Felt252Wrapper) -> Self {
         let felt: FieldElement = felt.into();
@@ -22,9 +51,27 @@ impl From<Felt252Wrapper> for H256 {
     }
 }
 
+impl TryFrom<H256> for Felt252Wrapper {
+    type Error = Felt252WrapperError;
+
+    fn try_from(h256: H256) -> Result<Self, Self::Error> {
+        let felt = FieldElement::from_bytes_be(&h256)?;
+        Ok(Self(felt))
+    }
+}
+
 impl From<Felt252Wrapper> for U256 {
     fn from(felt: Felt252Wrapper) -> Self {
         let felt: FieldElement = felt.into();
         U256::from_be_bytes(felt.to_bytes_be())
+    }
+}
+
+impl TryFrom<U256> for Felt252Wrapper {
+    type Error = Felt252WrapperError;
+
+    fn try_from(u256: U256) -> Result<Self, Self::Error> {
+        let felt = FieldElement::from_bytes_be(&u256.to_be_bytes())?;
+        Ok(Self(felt))
     }
 }
--- crates/core/src/models/mod.rs
+++ crates/core/src/models/mod.rs
@@ -6,12 +6,15 @@ pub mod transaction;
 
 use thiserror::Error;
 
+use self::felt::Felt252WrapperError;
 use crate::client::helpers::DataDecodingError;
 
 #[derive(Debug, Error)]
 pub enum ConversionError {
     #[error("transaction conversion error: {0}")]
     TransactionConversionError(String),
+    #[error(transparent)]
+    Felt252WrapperConversionError(#[from] Felt252WrapperError),
     #[error(transparent)]
     DataDecodingError(#[from] DataDecodingError),
 }
