3734b665806a1e41addfed1b699621f213f9b66c
bajpai244 <bajpaiharsh244@gmail.com>
2023-06-28 23:01:14 +0530

    refactor: use starknet event wrapper in `transaction_receipt`

--- crates/core/src/client/mod.rs
+++ crates/core/src/client/mod.rs
@@ -17,12 +17,11 @@ use helpers::{
 // TODO: all reth_primitives::rpc types should be replaced when native reth Log is implemented
 // https://github.com/paradigmxyz/reth/issues/1396#issuecomment-1440890689
 use reth_primitives::{
-    keccak256, Address, BlockId, BlockNumberOrTag, Bloom, Bytes, Bytes as RpcBytes, TransactionSigned, H160, H256,
-    U128, U256, U64, U8,
+    keccak256, Address, BlockId, BlockNumberOrTag, Bloom, Bytes, TransactionSigned, H256, U128, U256, U64, U8,
 };
 use reth_rlp::Decodable;
 use reth_rpc_types::{
-    BlockTransactions, CallRequest, FeeHistory, Index, Log, RichBlock, SyncInfo, SyncStatus,
+    BlockTransactions, CallRequest, FeeHistory, Index, RichBlock, SyncInfo, SyncStatus,
     Transaction as EtherTransaction, TransactionReceipt,
 };
 use starknet::core::types::{
@@ -44,7 +43,8 @@ use self::errors::EthApiError;
 use crate::client::constants::selectors::ETH_CALL;
 use crate::models::balance::{TokenBalance, TokenBalances};
 use crate::models::block::{BlockWithTxHashes, BlockWithTxs};
-use crate::models::convertible::{ConvertibleStarknetBlock, ConvertibleStarknetTransaction};
+use crate::models::convertible::{ConvertibleStarknetBlock, ConvertibleStarknetEvent, ConvertibleStarknetTransaction};
+use crate::models::event::StarknetEvent;
 use crate::models::felt::Felt252Wrapper;
 use crate::models::transaction::{StarknetTransaction, StarknetTransactions};
 
@@ -435,43 +435,9 @@ impl KakarotEthApi for KakarotClient<JsonRpcClient<HttpTransport>> {
 
                     // Cannot use `map` because of the `await` call.
                     for event in events {
-                        let contract_address = self.safe_get_evm_address(&event.from_address, &starknet_block_id).await;
-
-                        // event "keys" in cairo are event "topics" in solidity
-                        // they're returned as list where consecutive values are
-                        // low, high, low, high, etc. of the Uint256 Cairo representation
-                        // of the bytes32 topics. This recomputes the original topic
-                        let topics = (0..event.keys.len())
-                            .step_by(2)
-                            .map(|i| {
-                                let next_key = *event.keys.get(i + 1).unwrap_or(&FieldElement::ZERO);
-
-                                // Can unwrap here as we know 2^128 is a valid FieldElement
-                                let two_pow_16: FieldElement =
-                                    FieldElement::from_hex_be("0x100000000000000000000000000000000").unwrap();
-
-                                // TODO: May wrap around prime field - Investigate edge cases
-                                let felt_shifted_next_key = next_key * two_pow_16;
-                                event.keys[i] + felt_shifted_next_key
-                            })
-                            .map(|topic| H256::from(&topic.to_bytes_be()))
-                            .collect::<Vec<_>>();
-
-                        let data = vec_felt_to_bytes(event.data);
-
-                        let log = Log {
-                            // TODO: fetch correct address from Kakarot.
-                            // Contract Address is the account contract's address (EOA or KakarotAA)
-                            address: H160::from_slice(&contract_address.0),
-                            topics,
-                            data: RpcBytes::from(data.0),
-                            block_hash: None,
-                            block_number: None,
-                            transaction_hash: None,
-                            transaction_index: None,
-                            log_index: None,
-                            removed: false,
-                        };
+                        let event = StarknetEvent::new(event);
+                        let log =
+                            event.to_eth_log(self, block_hash, block_number, transaction_hash, None, None).await?;
 
                         logs.push(log);
                     }
