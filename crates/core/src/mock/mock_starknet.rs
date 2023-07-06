adeaa989a2ccc565d261b37ea76274673b78556c
Gregory Edison <gregory.edison1993@gmail.com>
2023-07-06 11:02:11 +0200

    add mocking for kakarot methods

--- crates/core/src/mock/mock_starknet.rs
+++ crates/core/src/mock/mock_starknet.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 use dojo_test_utils::rpc::MockJsonRpcTransport;
+use serde::{Deserialize, Serialize, Serializer};
 use serde_json::Value;
 use starknet::providers::jsonrpc::JsonRpcMethod;
 use starknet::providers::JsonRpcClient;
@@ -15,8 +16,50 @@ pub struct StarknetRpcFixture {
     response: Value,
 }
 
+#[derive(Debug, Deserialize)]
+pub enum KakarotJsonRpcMethod {
+    GetStarknetContractAddress,
+    ComputeStarknetAddress,
+    GetEvmAddress,
+    Other(JsonRpcMethod),
+}
+
+impl From<KakarotJsonRpcMethod> for JsonRpcMethod {
+    fn from(value: KakarotJsonRpcMethod) -> Self {
+        match value {
+            KakarotJsonRpcMethod::Other(method) => method,
+            _ => JsonRpcMethod::Call,
+        }
+    }
+}
+
+impl Serialize for KakarotJsonRpcMethod {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            KakarotJsonRpcMethod::GetStarknetContractAddress => {
+                serializer.serialize_str("kakarot_getStarknetContractAddress")
+            }
+            KakarotJsonRpcMethod::ComputeStarknetAddress => serializer.serialize_str("kakarot_computeStarknetAddress"),
+            KakarotJsonRpcMethod::GetEvmAddress => serializer.serialize_str("kakarot_getEvmAddress"),
+            KakarotJsonRpcMethod::Other(method) => method.serialize(serializer),
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! wrap_kakarot {
+    ($id:expr) => {
+        KakarotJsonRpcMethod::Other($id)
+    };
+}
+
 /// A builder for a `StarknetRpcFixture`.
 pub struct StarknetRpcFixtureBuilder {
+    /// The Kakarot Json RPC method.
+    method: KakarotJsonRpcMethod,
     /// The fixture to build.
     fixture: StarknetRpcFixture,
     /// The request loaded.
@@ -27,9 +70,14 @@ pub struct StarknetRpcFixtureBuilder {
 
 impl StarknetRpcFixtureBuilder {
     /// Returns a new `StarknetRpcFixtureBuilder`.
-    pub fn new(method: JsonRpcMethod) -> Self {
+    pub fn new(method: KakarotJsonRpcMethod) -> Self {
         Self {
-            fixture: StarknetRpcFixture { method, params: Value::Null, response: Value::Null },
+            method,
+            fixture: StarknetRpcFixture {
+                method: JsonRpcMethod::BlockNumber,
+                params: Value::Null,
+                response: Value::Null,
+            },
             request: Value::Null,
             response: Value::Null,
         }
@@ -40,11 +88,11 @@ impl StarknetRpcFixtureBuilder {
         let clean_quotations = |s: &str| s.replace('\"', "");
         let request_path = format!(
             "src/mock/fixtures/requests/{}.json",
-            clean_quotations(&serde_json::to_string(&self.fixture.method).unwrap())
+            clean_quotations(&serde_json::to_string(&self.method).unwrap())
         );
         let response_path = format!(
             "src/mock/fixtures/responses/{}.json",
-            clean_quotations(&serde_json::to_string(&self.fixture.method).unwrap())
+            clean_quotations(&serde_json::to_string(&self.method).unwrap())
         );
 
         self.request = fs::read_to_string(request_path).unwrap().parse::<Value>().unwrap();
@@ -67,7 +115,10 @@ impl StarknetRpcFixtureBuilder {
 
     /// Build the `StarknetRpcFixture`.
     pub fn build(self) -> StarknetRpcFixture {
-        self.fixture
+        let mut fixture = self.fixture;
+        fixture.method = self.method.into();
+        dbg!(&fixture.response, &fixture.params, &fixture.method);
+        fixture
     }
 }
 
@@ -77,7 +128,7 @@ impl StarknetRpcFixtureBuilder {
 /// # Arguments
 ///
 /// * `methods` - The json rpc methods to create fixtures for.
-pub fn fixtures(methods: Vec<JsonRpcMethod>) -> Vec<StarknetRpcFixture> {
+pub fn fixtures(methods: Vec<KakarotJsonRpcMethod>) -> Vec<StarknetRpcFixture> {
     methods
         .into_iter()
         .map(|method| StarknetRpcFixtureBuilder::new(method).load_jsons().with_params().with_response().build())
@@ -106,8 +157,10 @@ mod tests {
     #[test]
     fn test_rpc_fixture_builder() {
         // Given
-        let method = JsonRpcMethod::GetNonce;
+        let method = wrap_kakarot!(JsonRpcMethod::GetNonce);
         let fixture = StarknetRpcFixtureBuilder::new(method).load_jsons().with_params().with_response().build();
+
+        dbg!(&fixture.method, &fixture.params, &fixture.response);
 
         // When
         let expected_params = serde_json::json!(["latest", "0xabde1"]);
