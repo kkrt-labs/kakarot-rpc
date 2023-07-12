5f3463c5dc018610b9fc20ce751cb67741012730
johann makram salib bestowrous <jmsb@johanns-MBP.lan>
2023-07-12 03:29:00 -0700

    say the holy word 'todo' that allows us to forgive our hacks

--- crates/core/src/client/mod.rs
+++ crates/core/src/client/mod.rs
@@ -315,6 +315,8 @@ impl<P: Provider + Send + Sync> KakarotEthApi<P> for KakarotClient<P> {
 
                     TransactionReceipt {
                         transaction_hash,
+                        // TODO: transition this hardcoded default out of nearing-demo-day hack and seeing how to
+                        // properly source/translate this value
                         transaction_index: Some(U256::ZERO),
                         block_hash,
                         block_number,
@@ -495,6 +497,8 @@ impl<P: Provider + Send + Sync> KakarotEthApi<P> for KakarotClient<P> {
         let newest_block = U256::from(newest_block);
         let oldest_block: U256 = if newest_block >= block_count { newest_block - block_count } else { U256::from(0) };
 
+        // TODO: transition `reward` hardcoded default out of nearing-demo-day hack and seeing how to
+        // properly source/translate this value
         Ok(FeeHistory { base_fee_per_gas, gas_used_ratio, oldest_block, reward: Some(vec![vec![U256::ZERO]]) })
     }
 
