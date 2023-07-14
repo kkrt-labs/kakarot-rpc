c747beaf0ee4c446018de9f7dfc08273890d5148
Gregory Edison <gregory.edison1993@gmail.com>
2023-07-14 13:31:08 +0200

    add call wrapper

--- crates/core/src/models/call.rs
+++ crates/core/src/models/call.rs
@@ -2,22 +2,52 @@ use std::slice::SliceIndex;
 
 use reth_primitives::TransactionSigned;
 use reth_rlp::Decodable;
-use starknet::accounts::Call;
+use starknet::accounts::Call as StarknetCall;
 use starknet_crypto::FieldElement;
 
 use super::ConversionError;
 use crate::models::DataDecodingError;
 
-pub struct Calls(Vec<Call>);
+#[derive(Clone)]
+pub struct Call(StarknetCall);
 
-impl From<Calls> for Vec<Call> {
+impl From<Call> for StarknetCall {
+    fn from(call: Call) -> Self {
+        call.0
+    }
+}
+
+impl From<StarknetCall> for Call {
+    fn from(call: StarknetCall) -> Self {
+        Self(call)
+    }
+}
+
+impl From<Call> for Vec<FieldElement> {
+    fn from(call: Call) -> Self {
+        let mut c = vec![
+            FieldElement::ONE,
+            call.0.to,
+            call.0.selector,
+            FieldElement::ZERO,
+            FieldElement::from(call.0.calldata.len()),
+            FieldElement::from(call.0.calldata.len()),
+        ];
+        c.extend(call.0.calldata);
+        c
+    }
+}
+
+pub struct Calls(Vec<StarknetCall>);
+
+impl From<Calls> for Vec<StarknetCall> {
     fn from(calls: Calls) -> Self {
         calls.0
     }
 }
 
-impl From<Vec<Call>> for Calls {
-    fn from(calls: Vec<Call>) -> Self {
+impl From<Vec<StarknetCall>> for Calls {
+    fn from(calls: Vec<StarknetCall>) -> Self {
         Self(calls)
     }
 }
@@ -41,7 +71,7 @@ impl TryFrom<Vec<FieldElement>> for Calls {
         for i in 0..calls_len {
             let calldata_len =
                 u32::try_from(value[i * 4 + 4]).map_err(|e| ConversionError::ValueOutOfRange(e.to_string()))? as usize;
-            let call = Call {
+            let call = StarknetCall {
                 to: value[i * 4 + 2],
                 selector: value[i * 4 + 3],
                 calldata: value[offset..offset + calldata_len].to_vec(),
@@ -84,7 +114,7 @@ impl Calls {
 
     pub fn get<I>(&self, index: I) -> Option<&I::Output>
     where
-        I: SliceIndex<[Call]>,
+        I: SliceIndex<[StarknetCall]>,
     {
         self.0.get(index)
     }
@@ -98,10 +128,43 @@ mod tests {
     use serde::Deserialize;
 
     use super::*;
+    use crate::client::constants::selectors::ETH_CALL;
+    use crate::mock::constants::ACCOUNT_ADDRESS;
 
     #[derive(Deserialize)]
     struct SerdeCall {
         pub calldata: Vec<FieldElement>,
+    }
+
+    #[test]
+    fn test_from_call() {
+        // Given
+        let call: Call = StarknetCall {
+            to: *ACCOUNT_ADDRESS,
+            selector: ETH_CALL,
+            calldata: vec![1u8, 2u8, 3u8, 4u8, 5u8, 6u8].into_iter().map(FieldElement::from).collect(),
+        }
+        .into();
+
+        // When
+        let raw_calldata = Vec::<FieldElement>::from(call);
+
+        // Then
+        let expected = vec![
+            FieldElement::ONE,
+            *ACCOUNT_ADDRESS,
+            ETH_CALL,
+            FieldElement::ZERO,
+            FieldElement::from(6u8),
+            FieldElement::from(6u8),
+            FieldElement::from(1u8),
+            FieldElement::from(2u8),
+            FieldElement::from(3u8),
+            FieldElement::from(4u8),
+            FieldElement::from(5u8),
+            FieldElement::from(6u8),
+        ];
+        assert_eq!(expected, raw_calldata);
     }
 
     #[test]
