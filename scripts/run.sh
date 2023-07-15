de60b8373cfdf90f3145f5f377cc4d0e609d719c
Fred Tupas <fred.tupas21@gmail.com>
2023-07-15 15:20:25 +0200

    feat: add docker-compose for Katana + Kakarot + RPC

---
+++ scripts/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script is used to set the environment variables for the Kakarot RPC
+export KAKAROT_ADDRESS=$(jq -r '.kakarot.address' ./deployments/${STARKNET_NETWORK}/deployments.json)
+export PROXY_ACCOUNT_CLASS_HASH=$(jq -r '.proxy' ./deployments/${STARKNET_NETWORK}/declarations.json)
+
+echo "Kakarot address: $KAKAROT_ADDRESS"
+echo "Proxy account class hash: $PROXY_ACCOUNT_CLASS_HASH"
+
+# Run the command passed to the docker run
+exec "$@"
