f63b20e9c76c86d54a691aaacf3b1881a04b3a3e
Danilo Woo Hyung Kim <danilowoohyungkim@danilos-mbp-2.home>
2023-07-26 17:43:25 +0200

    testing opcodeCall and opcodeStaticCall

--- scripts/PlainOpcodes.s.sol
+++ scripts/PlainOpcodes.s.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "kakarot/PlainOpcodes/Counter.sol";
+import "kakarot/PlainOpcodes/GetCaller.sol";
 import "kakarot/PlainOpcodes/PlainOpcodes.sol";
+import "kakarot/PlainOpcodes/Counter.sol";
 
 contract PlainOpcodesScript is Script {
     Counter public counter;
     PlainOpcodes public plainOpcodes;
+    GetCaller public getCaller;
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("EVM_PRIVATE_KEY");
@@ -15,6 +17,17 @@ contract PlainOpcodesScript is Script {
 
         counter = new Counter();
         plainOpcodes = new PlainOpcodes(address(counter));
+        getCaller = new GetCaller();
+
+        counter.inc();
+        plainOpcodes.opcodeCall();
+
+        uint256 counter_value = plainOpcodes.opcodeStaticCall();
+        address sender_address = getCaller.getCallerAddress();
+
+        console.logAddress(sender_address);
+        console.logUint(counter_value);
+        require(counter_value == 2, "Counter should be 4");
 
         vm.stopBroadcast();
     }
