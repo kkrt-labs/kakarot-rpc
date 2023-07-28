b8e7f3a5d6f0e802c549e775643de6b8913e6a8f
Gregory Edison <gregory.edison1993@gmail.com>
2023-07-28 17:35:00 +0200

    add future token balance

--- crates/core/src/models/balance.rs
+++ crates/core/src/models/balance.rs
@@ -1,3 +1,7 @@
+use std::pin::{pin, Pin};
+use std::task::Poll;
+
+use futures::Future;
 use reth_primitives::{Address, U256};
 use serde::{Deserialize, Serialize};
 
@@ -12,4 +16,26 @@ pub struct TokenBalance {
 pub struct TokenBalances {
     pub address: Address,
     pub token_balances: Vec<TokenBalance>,
+}
+
+#[pin_project::pin_project]
+pub struct FutureTokenBalance<F: Future> {
+    #[pin]
+    pub balance: F,
+    pub token_address: Address,
+}
+
+impl<F: Future> Future for FutureTokenBalance<F> {
+    type Output = (F::Output, Address);
+
+    fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+        let mut this = self.project();
+        let balance = this.balance.as_mut().poll(cx);
+        let token_address = this.token_address.to_owned();
+
+        match balance {
+            Poll::Ready(output) => Poll::Ready((output, token_address)),
+            Poll::Pending => Poll::Pending,
+        }
+    }
 }
