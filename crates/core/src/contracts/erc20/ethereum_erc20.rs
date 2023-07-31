43f193fa14d5d9af9378d442a08b4d4f010463ff
Gregory Edison <gregory.edison1993@gmail.com>
2023-07-31 16:00:58 +0200

    introduce abigen!

--- crates/core/src/contracts/erc20/ethereum_erc20.rs
+++ crates/core/src/contracts/erc20/ethereum_erc20.rs
@@ -1,6 +1,6 @@
-use ethers::abi::{AbiParser, Contract, Token};
+use ethers::abi::AbiEncode;
+use ethers::prelude::abigen;
 use ethers::types::Address;
-use lazy_static::lazy_static;
 use reth_primitives::{BlockId, U256};
 use starknet::core::types::BlockId as StarknetBlockId;
 use starknet::providers::Provider;
@@ -11,19 +11,15 @@ use crate::client::helpers::DataDecodingError;
 use crate::contracts::kakarot::KakarotContract;
 use crate::models::block::EthBlockId;
 
-lazy_static! {
-    /// Abi linkage for Erc20 functions and events
-    static ref ABI: Contract = AbiParser::default().parse_str(r#"[
-        function totalSupply() external view returns (uint256)
+// abigen generates a lot of unused code, needs to be benchmarked if performances ever become a
+// concern
+abigen!(
+    IERC20,
+    r#"[
         function balanceOf(address account) external view returns (uint256)
-        function transfer(address recipient, uint256 amount) external returns (bool)
         function allowance(address owner, address spender) external view returns (uint256)
-        function approve(address spender, uint256 amount) external returns (bool)
-        function transferFrom( address sender, address recipient, uint256 amount) external returns (bool)
-        event Transfer(address indexed from, address indexed to, uint256 value)
-        event Approval(address indexed owner, address indexed spender, uint256 value)
-    ]"#).unwrap();
-}
+    ]"#,
+);
 
 /// Abstraction for a Kakarot ERC20 contract.
 pub struct EthereumErc20<'a, P> {
@@ -38,11 +34,7 @@ impl<'a, P: Provider + Send + Sync> EthereumErc20<'a, P> {
 
     pub async fn balance_of(self, evm_address: Address, block_id: BlockId) -> Result<U256, EthApiError<P::Error>> {
         // Prepare the calldata for the bytecode function call
-        let calldata = ABI
-            .function("balanceOf")
-            .map_err(|err| EthApiError::ContractError(err.into()))?
-            .encode_input(&[Token::Address(evm_address)])
-            .map_err(|err| EthApiError::ContractError(err.into()))?;
+        let calldata = IERC20Calls::BalanceOf(BalanceOfCall { account: evm_address }).encode();
         let calldata = calldata.into_iter().map(FieldElement::from).collect();
 
         let block_id = EthBlockId::new(block_id);
--- crates/core/src/contracts/erc20/mod.rs
+++ crates/core/src/contracts/erc20/mod.rs
@@ -1,13 +1,2 @@
 pub mod ethereum_erc20;
 pub mod starknet_erc20;
-
-use ethers::abi::Error as AbiError;
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-/// Contract Error
-pub enum ContractError {
-    /// Contract Abi error
-    #[error(transparent)]
-    AbiError(#[from] AbiError),
-}
