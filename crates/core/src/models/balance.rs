4180b754e43d7ac1bc8db0e8a6729cfaadf4e741
Gregory Edison <gregory.edison1993@gmail.com>
2023-07-31 13:53:58 +0200

    update future output

--- crates/core/src/models/balance.rs
+++ crates/core/src/models/balance.rs
@@ -1,13 +1,17 @@
+use std::marker::PhantomData;
 use std::pin::{pin, Pin};
 use std::task::Poll;
 
 use futures::Future;
 use reth_primitives::{Address, U256};
 use serde::{Deserialize, Serialize};
+use starknet::providers::Provider;
+
+use crate::client::errors::EthApiError;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct TokenBalance {
-    pub contract_address: Address,
+    pub token_address: Address,
     pub token_balance: Option<U256>,
     pub error: Option<String>,
 }
@@ -18,15 +22,18 @@ pub struct TokenBalances {
     pub token_balances: Vec<TokenBalance>,
 }
 
+type BalanceOfResult<P> = Result<U256, EthApiError<<P as Provider>::Error>>;
+
 #[pin_project::pin_project]
-pub struct FutureTokenBalance<F: Future> {
+pub struct FutureTokenBalance<P: Provider, F: Future<Output = BalanceOfResult<P>>> {
     #[pin]
     pub balance: F,
     pub token_address: Address,
+    pub(crate) _phantom: PhantomData<P>,
 }
 
-impl<F: Future> Future for FutureTokenBalance<F> {
-    type Output = (F::Output, Address);
+impl<P: Provider, F: Future<Output = BalanceOfResult<P>>> Future for FutureTokenBalance<P, F> {
+    type Output = TokenBalance;
 
     fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
         let mut this = self.project();
@@ -34,7 +41,12 @@ impl<F: Future> Future for FutureTokenBalance<F> {
         let token_address = this.token_address.to_owned();
 
         match balance {
-            Poll::Ready(output) => Poll::Ready((output, token_address)),
+            Poll::Ready(output) => match output {
+                Ok(balance) => Poll::Ready(TokenBalance { token_address, token_balance: Some(balance), error: None }),
+                Err(error) => {
+                    Poll::Ready(TokenBalance { token_address, token_balance: None, error: Some(error.to_string()) })
+                }
+            },
             Poll::Pending => Poll::Pending,
         }
     }
