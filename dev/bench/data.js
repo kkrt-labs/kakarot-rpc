331a713e307663037d86d6f89f0b542a06452b18
github-action-benchmark <github@users.noreply.github.com>
2023-08-10 10:18:43 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for bd485c4618ebd0f8f08fad7c5d4249d45d3547a5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,8 +1,41 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691662721038,
+  "lastUpdate": 1691662723723,
   "repoUrl": "https://github.com/kkrt-labs/kakarot-rpc",
   "entries": {
     "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "bajpaiharsh244@gmail.com",
+            "name": "Harsh Bajpai",
+            "username": "bajpai244"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bd485c4618ebd0f8f08fad7c5d4249d45d3547a5",
+          "message": "feat: add native token transfer benchmarks (#324)\n\nfeat: add benchmark to CI",
+          "timestamp": "2023-08-10T08:58:41Z",
+          "tree_id": "9eb1a64db8244f8f1e7b04da0d4d9a6bd8051b43",
+          "url": "https://github.com/kkrt-labs/kakarot-rpc/commit/bd485c4618ebd0f8f08fad7c5d4249d45d3547a5"
+        },
+        "date": 1691662720237,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "transactions per block",
+            "value": "transactions/block"
+          },
+          {
+            "name": "transactions per second",
+            "value": 1.2,
+            "unit": "transactions/second"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
