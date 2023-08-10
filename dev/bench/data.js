023dde3c718aea567e80f5c6c54087e6ae087ba5
github-action-benchmark <github@users.noreply.github.com>
2023-08-10 10:50:11 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 9696424fe509341306cd434ad015c9b5822a851c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691664607296,
+  "lastUpdate": 1691664611832,
   "repoUrl": "https://github.com/kkrt-labs/kakarot-rpc",
   "entries": {
     "Benchmark": [
@@ -65,6 +65,39 @@ window.BENCHMARK_DATA = {
           {
             "name": "transactions per second",
             "value": 1.2,
+            "unit": "transactions/second"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "35031356+ftupas@users.noreply.github.com",
+            "name": "ftupas",
+            "username": "ftupas"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9696424fe509341306cd434ad015c9b5822a851c",
+          "message": "feat: add `rstest` test framework (#399)\n\n* feat: add rstest fixtures\n\n* test: use rstest fixtures\n\n* feat: add parameterized test using rstest\n\n* dev: remove drop and println\n\n* refactor: use KakarotTestEnvironmentContext instead of KakarotTestEnvironment\n\n* dev: remove more boilerplate code",
+          "timestamp": "2023-08-10T09:14:39Z",
+          "tree_id": "928e0a436a521597badc92d090f2128224c34e8b",
+          "url": "https://github.com/kkrt-labs/kakarot-rpc/commit/9696424fe509341306cd434ad015c9b5822a851c"
+        },
+        "date": 1691664605692,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "transactions per block",
+            "value": "transactions/block"
+          },
+          {
+            "name": "transactions per second",
+            "value": 1.25,
             "unit": "transactions/second"
           }
         ]
