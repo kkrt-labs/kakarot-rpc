266e0462cfde9d2fbc65b50dd0761db676a159fb
Gregory Edison <gregory.edison1993@gmail.com>
2023-08-16 13:38:25 +0200

    add convertible event filter

--- crates/core/src/models/convertible.rs
+++ crates/core/src/models/convertible.rs
@@ -1,10 +1,12 @@
 use async_trait::async_trait;
 use reth_primitives::{H256, U256};
-use reth_rpc_types::{Log, RichBlock, Transaction as EthTransaction};
+use reth_rpc_types::{Log, RichBlock, Transaction as EthTransaction, TransactionReceipt};
+use starknet::core::types::EventFilter;
 use starknet::providers::Provider;
 
 use crate::client::api::{KakarotEthApi, KakarotStarknetApi};
 use crate::client::errors::EthApiError;
+use crate::client::KakarotClient;
 
 #[async_trait]
 pub trait ConvertibleStarknetBlock {
@@ -23,6 +25,13 @@ pub trait ConvertibleStarknetEvent {
     ) -> Result<Log, EthApiError<P::Error>>;
 }
 
+pub trait ConvertibleEthEventFilter {
+    fn to_starknet_event_filter<P: Provider + Send + Sync>(
+        self,
+        client: &KakarotClient<P>,
+    ) -> Result<EventFilter, EthApiError<P::Error>>;
+}
+
 #[async_trait]
 pub trait ConvertibleStarknetTransaction {
     async fn to_eth_transaction<P: Provider + Send + Sync>(
@@ -32,4 +41,12 @@ pub trait ConvertibleStarknetTransaction {
         block_number: Option<U256>,
         transaction_index: Option<U256>,
     ) -> Result<EthTransaction, EthApiError<P::Error>>;
+}
+
+#[async_trait]
+pub trait ConvertibleStarknetTransactionReceipt {
+    async fn to_eth_transaction_receipt<P: Provider + Send + Sync>(
+        self,
+        client: &KakarotClient<P>,
+    ) -> Result<Option<TransactionReceipt>, EthApiError<P::Error>>;
 }
