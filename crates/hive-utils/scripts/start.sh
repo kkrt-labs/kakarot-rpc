27e0385c5b88ba6826dba8ff5ca67495ad53a9bb
Fred Tupas <fred.tupas21@gmail.com>
2023-08-22 16:09:32 +0200

    dev: add loop to check Madara

--- crates/hive-utils/scripts/start.sh
+++ crates/hive-utils/scripts/start.sh
@@ -11,6 +11,21 @@ export PROXY_ACCOUNT_CLASS_HASH=$(starkli class-hash ${MADARA_PATH}/cairo-contra
     --tmp \
     --dev &
 
-# Wait for Madara to start
-# Start RPC
-sleep 20 && /usr/local/bin/kakarot-rpc
+# Loop until the curl command succeeds
+until 
+  curl --silent --request POST \
+       --header "Content-Type: application/json" \
+       --data '{
+           "jsonrpc": "2.0", 
+           "method": "starknet_getClassHashAt", 
+           "params": [{"block_number": 0}, "0x9001"], 
+           "id": 1
+       }' \
+       "${STARKNET_NETWORK}" # Use the provided network address
+do
+    echo "Waiting for Madara to start..."
+    sleep 5
+done
+
+# Once Madara is ready, start RPC
+/usr/local/bin/kakarot-rpc
