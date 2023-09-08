8ace888026530076ac6c5e6f856b10ab30f5ec0d
Gregory Edison <gregory.edison1993@gmail.com>
2023-09-08 11:03:44 +0200

    add waiter to kakarot contract

--- crates/core/src/contracts/kakarot.rs
+++ crates/core/src/contracts/kakarot.rs
@@ -10,7 +10,7 @@ use starknet_crypto::FieldElement;
 use crate::client::constants::selectors::{COMPUTE_STARKNET_ADDRESS, DEPLOY_EXTERNALLY_OWNED_ACCOUNT, ETH_CALL};
 use crate::client::errors::EthApiError;
 use crate::client::helpers::{decode_eth_call_return, vec_felt_to_bytes, DataDecodingError};
-use crate::test_utils::execution_helpers::wait_for_tx;
+use crate::client::waiter::TransactionWaiter;
 
 pub struct KakarotContract<P> {
     pub address: FieldElement,
@@ -86,10 +86,13 @@ impl<P: Provider + Send + Sync + 'static> KakarotContract<P> {
             .send()
             .await;
 
-        wait_for_tx();
-
         let result: Result<FieldElement, EthApiError<P::Error>> = match result {
-            Ok(invoke_result) => Ok(invoke_result.transaction_hash),
+            Ok(invoke_result) => {
+                let waiter =
+                    TransactionWaiter::new(self.provider.clone(), invoke_result.transaction_hash, 1000, 15_000);
+                waiter.poll().await?;
+                Ok(invoke_result.transaction_hash)
+            }
             Err(error) => match error {
                 AccountError::Provider(error) => Err(EthApiError::from(error)),
                 _ => Err(EthApiError::Other(error.into())),
