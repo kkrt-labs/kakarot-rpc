bc367a28dabe5d9dd86bee2021d8e59d1fa7e87e
TAdev0 <angles.tristan@protonmail.com>
2023-11-27 22:53:25 +0100

    KakarotRPCHealthCheck

--- crates/eth-rpc/src/servers/net_rpc.rs
+++ crates/eth-rpc/src/servers/net_rpc.rs
@@ -2,16 +2,19 @@ use jsonrpsee::core::{async_trait, RpcResult as Result};
 use kakarot_rpc_core::client::constants::CHAIN_ID;
 use reth_primitives::U64;
 use reth_rpc_types::PeerCount;
+use std::io;
 
 use crate::api::net_api::NetApiServer;
 
 /// The RPC module for the implementing Net api
 #[derive(Default)]
-pub struct NetRpc {}
+pub struct NetRpc<P: Provider + Send + Sync> {
+    pub kakarot_client: Arc<KakarotClient<P>>,
+}
 
-impl NetRpc {
-    pub const fn new() -> Self {
-        Self {}
+impl<P: Provider + Send + Sync> NetRpc<P> {
+    pub fn new(kakarot_client: Arc<KakarotClient<P>>) -> Self {
+        Self { kakarot_client }
     }
 }
 
@@ -31,7 +34,11 @@ impl NetApiServer for NetRpc {
         Ok(false)
     }
 
-    fn health(&self) -> Result<bool> {
-        Ok(true)
+    fn health(&self) -> Result<bool, io::Error> {
+        // call `starknet_blockNumber` function to check if it resolves
+        match self.kakarot_client.starknet_blockNumber() {
+            Ok(_) => Ok(true),
+            Err(_) => Err(io::Error::new(io::ErrorKind::NotFound, "Kakarot RPC currently unreacheable")),
+        }
     }
 }
