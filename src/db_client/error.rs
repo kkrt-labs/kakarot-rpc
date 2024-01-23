7112e1c8a2c3b673f3c253fc63da21ac33d54786
Gregory Edison <gregory.edison1993@gmail.com>
2024-01-23 19:59:31 +0100

    add db client and db types

---
+++ src/db_client/error.rs
@@ -0,0 +1,27 @@
+use jsonrpsee::types::ErrorObject;
+use thiserror::Error;
+
+use crate::starknet_client::errors::{rpc_err, EthRpcErrorCode};
+
+/// Error that can occur when interacting with the database.
+#[derive(Debug, Error)]
+pub enum DatabaseError {
+    /// MongoDB error.
+    #[error("MongoDB error: {0}")]
+    MongoDbError(#[from] mongodb::error::Error),
+    /// Value not found in the database.
+    #[error("Did not find value in the database.")]
+    ValueNotFound,
+}
+
+impl From<DatabaseError> for jsonrpsee::core::Error {
+    fn from(err: DatabaseError) -> Self {
+        Self::Call(err.into())
+    }
+}
+
+impl From<DatabaseError> for ErrorObject<'static> {
+    fn from(value: DatabaseError) -> Self {
+        rpc_err(EthRpcErrorCode::InternalError, value.to_string())
+    }
+}
