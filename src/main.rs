649e26dd43430876201cad3cf17c4f9cb3ccd975
Gregory Edison <gregory.edison1993@gmail.com>
2024-01-23 20:00:03 +0100

    implement first routes with db

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use std::sync::Arc;
 
 use dotenv::dotenv;
 use eyre::Result;
+use kakarot_rpc::db_client::client::DbClient;
 use kakarot_rpc::eth_rpc::config::RPCConfig;
 use kakarot_rpc::eth_rpc::rpc::KakarotRpcModuleBuilder;
 use kakarot_rpc::eth_rpc::run_server;
 use kakarot_rpc::starknet_client::config::{
-    JsonRpcClientBuilder, KakarotRpcConfig, Network, SequencerGatewayProviderBuilder,
+    env_var, JsonRpcClientBuilder, KakarotRpcConfig, Network, SequencerGatewayProviderBuilder,
 };
 use kakarot_rpc::starknet_client::KakarotClient;
+use mongodb::options::{DatabaseOptions, ReadConcern, WriteConcern};
 use starknet::providers::jsonrpc::HttpTransport;
 use starknet::providers::{JsonRpcClient, SequencerGatewayProvider};
 use tracing_subscriber::util::SubscriberInitExt;
@@ -22,7 +24,6 @@ enum StarknetProvider {
 async fn main() -> Result<()> {
     dotenv().ok();
     // Environment variables are safe to use after this
-
     let filter = tracing_subscriber::EnvFilter::try_from_default_env()?;
     tracing_subscriber::FmtSubscriber::builder().with_env_filter(filter).finish().try_init()?;
 
@@ -42,16 +43,25 @@ async fn main() -> Result<()> {
         ),
     };
 
+    let db_client =
+        mongodb::Client::with_uri_str(env_var("MONGO_CONNECTION_STRING").expect("Missing MONGO_CONNECTION_STRING"))
+            .await?;
+    let db = db_client.database_with_options(
+        "Kakarot-Testnet-0",
+        DatabaseOptions::builder().read_concern(ReadConcern::majority()).write_concern(WriteConcern::MAJORITY).build(),
+    );
+    let db_client = DbClient::new(db);
+
     let kakarot_rpc_module = match starknet_provider {
         StarknetProvider::JsonRpcClient(starknet_provider) => {
             let starknet_provider = Arc::new(starknet_provider);
             let kakarot_client = Arc::new(KakarotClient::new(starknet_config, starknet_provider));
-            KakarotRpcModuleBuilder::new(kakarot_client).rpc_module()
+            KakarotRpcModuleBuilder::new(kakarot_client, db_client).rpc_module()
         }
         StarknetProvider::SequencerGatewayProvider(starknet_provider) => {
             let starknet_provider = Arc::new(starknet_provider);
             let kakarot_client = Arc::new(KakarotClient::new(starknet_config, starknet_provider));
-            KakarotRpcModuleBuilder::new(kakarot_client).rpc_module()
+            KakarotRpcModuleBuilder::new(kakarot_client, db_client).rpc_module()
         }
     }?;
 
