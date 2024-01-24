84012470dcef53102254a5e8af235413014c1773
Gregory Edison <gregory.edison1993@gmail.com>
2024-01-24 19:01:42 +0100

    add EthereumProvider trait + mocking

--- src/storage/types/transaction.rs
+++ src/storage/types/transaction.rs
@@ -2,12 +2,14 @@ use reth_primitives::H256;
 use reth_rpc_types::Transaction;
 use serde::Deserialize;
 
+/// A full transaction as stored in the database
 #[derive(Debug, Deserialize)]
 pub struct StoredTransactionFull {
     #[serde(deserialize_with = "crate::storage::types::serde::deserialize_intermediate")]
     pub tx: Transaction,
 }
 
+/// A transaction hash as stored in the database
 #[derive(Debug, Deserialize)]
 pub struct StoredTransactionHash {
     pub tx: Hash,
