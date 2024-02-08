c6d69433892968cf6ec7bac688c35b413e0de6e8
Elias Tazartes <elias.tazartes@gmail.com>
2024-02-08 11:35:23 +0100

    feat: first commit to get things started

---
+++ docker/hive/start.sh
@@ -0,0 +1,5 @@
+# 1. Convert the genesis.json from hive format to Katana format using the convert binary from Kakarot RPC test utils
+# 2. Start the Katana, the CairoVM chain
+###### 2.5. Await Katana to be healthy
+# 3. Start the Indexer service: DNA Indexer, Indexer transformer, and MongoDB
+# 4. Start the Kakarot RPC service
