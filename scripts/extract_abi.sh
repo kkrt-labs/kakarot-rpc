c2d98d04180111a8b6e3cb76b29175d5731eef39
Elias Tazartes <elias.tazartes@gmail.com>
2024-03-05 17:28:15 +0100

    fix build copy

--- scripts/extract_abi.sh
+++ scripts/extract_abi.sh
@@ -4,12 +4,16 @@ set -o pipefail
 # Directory containing the original JSON files
 src_dir="./lib/kakarot/build"
 
+# Root build directory
+root_dir="./.kakarot"
 # Directory to write the new JSON files to
-dest_dir="./artifacts"
+dest_dir="/artifacts"
+# Directory to write the entire build to
+build_dir="/build"
 
 # Ensure the destination directory exists
-rm -rf "${dest_dir}"
-mkdir -p "${dest_dir}"
+rm -rf "${root_dir}"
+mkdir -p "${root_dir}"
 
 # Find and process each JSON file
 find "${src_dir}" -type f -name '*.json' | while read -r src_file; do
@@ -20,8 +24,12 @@ find "${src_dir}" -type f -name '*.json' | while read -r src_file; do
 	# Check and create a subdirectory structure in destination if needed
 	sub_dir=$(dirname "${src_file}")
 	sub_dir=${sub_dir#"${src_dir}"}
-	mkdir -p "${dest_dir}${sub_dir}"
+	mkdir -p "${root_dir}${dest_dir}${sub_dir}"
+	mkdir -p "${root_dir}${build_dir}${sub_dir}"
 
 	# Use jq to extract the 'abi' field and write it to a new JSON file
-	jq '.abi' "${src_file}" >"${dest_dir}${sub_dir}/${filename}.json"
+	jq '.abi' "${src_file}" >"${root_dir}${dest_dir}${sub_dir}/${filename}.json"
+
+	# Copy the original JSON file to the build directory
+	cp "${src_file}" "${root_dir}${build_dir}${sub_dir}/${filename}.json"
 done
