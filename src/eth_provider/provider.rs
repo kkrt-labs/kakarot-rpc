1f0c32b6926a5ef2c291342fca77c75f872959fb
Thomas Coratger <thomas.coratger@gmail.com>
2024-04-15 17:04:43 +0200

    randomize test_block_by_hash

--- src/eth_provider/provider.rs
+++ src/eth_provider/provider.rs
@@ -451,7 +451,7 @@ where
                 .insert("log.address", doc! {"$in": adds.into_iter().map(|a| format_hex(a, 40)).collect::<Vec<_>>()})
         });
 
-        let logs: Vec<StoredLog> = self.database.get(database_filter, None).await?;
+        let logs = self.database.get::<StoredLog>(database_filter, None).await?;
         Ok(FilterChanges::Logs(logs.into_iter().map_into().collect()))
     }
 
@@ -504,7 +504,7 @@ where
                 };
                 gas_used / gas_limit
             })
-            .collect::<Vec<_>>();
+            .collect();
 
         let mut base_fee_per_gas =
             blocks.iter().map(|header| header.header.base_fee_per_gas.unwrap_or_default()).collect::<Vec<_>>();
@@ -617,20 +617,17 @@ where
         &self,
         block_id: Option<BlockId>,
     ) -> EthProviderResult<Option<Vec<reth_rpc_types::Transaction>>> {
-        let block_id = block_id.unwrap_or(BlockId::Number(BlockNumberOrTag::Latest));
-        let block_id = match block_id {
+        let block_id = match block_id.unwrap_or(BlockId::Number(BlockNumberOrTag::Latest)) {
             BlockId::Number(maybe_number) => {
                 BlockHashOrNumber::Number(self.tag_into_block_number(maybe_number).await?.to())
             }
             BlockId::Hash(hash) => BlockHashOrNumber::Hash(hash.block_hash),
         };
-        let block_exists = self.block_exists(block_id).await?;
-        if !block_exists {
+        if !self.block_exists(block_id).await? {
             return Ok(None);
         }
 
-        let transactions = self.transactions(block_id, true).await?;
-        match transactions {
+        match self.transactions(block_id, true).await? {
             BlockTransactions::Full(transactions) => Ok(Some(transactions)),
             _ => Err(TransactionError::ExpectedFullTransactions.into()),
         }
