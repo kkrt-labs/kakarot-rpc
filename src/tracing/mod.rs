cce32e3ec0f58169d531aa310d148cf9a77c8e35
Gregory Edison <gregory.edison1993@gmail.com>
2024-04-17 12:11:25 +0200

    refactor tracing

--- src/tracing/mod.rs
+++ src/tracing/mod.rs
@@ -1,18 +1,19 @@
 mod database;
 
-use crate::eth_provider::{
-    error::{EthApiError, TransactionError},
-    provider::EthereumProvider,
+use crate::{
+    eth_provider::{
+        error::{EthApiError, SignatureError, TransactionError},
+        provider::EthereumProvider,
+    },
+    models::transaction::rpc_to_primitive_transaction,
 };
 use reth_primitives::{
+    revm::env::tx_env_with_recovered,
     revm_primitives::{BlockEnv, CfgEnv, Env, EnvWithHandlerCfg, SpecId},
-    B256, U256,
+    Signature, TransactionSigned, B256, U256,
 };
 use reth_revm::tracing::{TracingInspector, TracingInspectorConfig};
-use reth_revm::{
-    primitives::{HandlerCfg, TransactTo},
-    DatabaseCommit,
-};
+use reth_revm::{primitives::HandlerCfg, DatabaseCommit};
 use reth_rpc_types::{trace::parity::LocalizedTransactionTrace, BlockId, BlockTransactions, Header, TransactionInfo};
 
 pub type TracerResult<T> = Result<T, EthApiError>;
@@ -35,6 +36,7 @@ impl<P: EthereumProvider + Send + Sync + Clone> Tracer<P> {
     }
 
     pub async fn trace_block(&self, block_id: BlockId) -> TracerResult<Option<Vec<LocalizedTransactionTrace>>> {
+        // TODO: split the function in smaller batches.
         let maybe_block = match block_id {
             BlockId::Hash(hash) => self.eth_provider.block_by_hash(hash.block_hash, true).await?,
             BlockId::Number(number) => self.eth_provider.block_by_number(number, true).await?,
@@ -44,8 +46,6 @@ impl<P: EthereumProvider + Send + Sync + Clone> Tracer<P> {
             return Ok(None);
         }
 
-        dbg!(&maybe_block);
-
         let block = maybe_block.unwrap();
         if block.header.hash.unwrap_or_default().is_zero() {
             return Err(EthApiError::UnknownBlock);
@@ -53,6 +53,7 @@ impl<P: EthereumProvider + Send + Sync + Clone> Tracer<P> {
 
         let Header { number, timestamp, gas_limit, miner, base_fee_per_gas, difficulty, .. } = block.header;
 
+        // TODO: should block env be the previous block?
         let block_env = BlockEnv {
             number: number.unwrap_or_default().to(),
             timestamp,
@@ -74,62 +75,54 @@ impl<P: EthereumProvider + Send + Sync + Clone> Tracer<P> {
         handler.append_handler_register_plain(reth_revm::inspector_handle_register);
         let mut evm = reth_revm::Evm::new(ctx, handler);
 
-        dbg!("entering tracing");
-
         let traces = tokio::task::block_in_place(move || {
             let transactions = match &block.transactions {
                 BlockTransactions::Full(transactions) => transactions,
                 _ => return Err(TransactionError::ExpectedFullTransactions.into()),
             };
+            let mut transactions = transactions.iter().peekable();
             let mut traces = Vec::with_capacity(block.transactions.len());
 
-            for tx in transactions {
-                dbg!(tx);
-                evm = evm
-                    .modify()
-                    .modify_tx_env(|tx_env| {
-                        tx_env.caller = tx.from;
-                        tx_env.gas_limit = tx.gas.to();
-                        tx_env.gas_price = tx.gas_price.unwrap_or_default();
-                        tx_env.transact_to = tx.to.map(TransactTo::call).unwrap_or_else(TransactTo::create);
-                        tx_env.value = tx.value;
-                        tx_env.data = tx.input.clone();
-                        tx_env.nonce = Some(tx.nonce);
-                        tx_env.chain_id = tx.chain_id;
-                        tx_env.access_list = tx
-                            .access_list
-                            .clone()
-                            .map(|al| {
-                                al.0.into_iter()
-                                    .map(|item| {
-                                        (
-                                            item.address,
-                                            item.storage_keys
-                                                .into_iter()
-                                                .map(|slot| U256::from_be_bytes(slot.0))
-                                                .collect(),
-                                        )
-                                    })
-                                    .collect()
-                            })
-                            .unwrap_or_default();
-                        tx_env.gas_priority_fee = tx.max_priority_fee_per_gas;
-                    })
-                    .build();
-                let result = evm.transact().map_err(|err| TransactionError::TracingError(err.into()))?;
-                evm.context.evm.inner.db.commit(result.state);
+            while let Some(tx) = transactions.next() {
+                // Recover the transaction
+                let transaction = rpc_to_primitive_transaction(tx.clone())?;
+                let signature = tx.signature.ok_or(SignatureError::MissingSignature)?;
+                let tx_signed = TransactionSigned::from_transaction_and_signature(
+                    transaction,
+                    Signature {
+                        r: signature.r,
+                        s: signature.s,
+                        odd_y_parity: signature.y_parity.ok_or(SignatureError::RecoveryError)?.0,
+                    },
+                );
+                let tx_ec_recovered = tx_signed.try_into_ecrecovered().map_err(|_| SignatureError::RecoveryError)?;
+                let tx_env = tx_env_with_recovered(&tx_ec_recovered);
+                // Update the transaction environment
+                evm = evm.modify().modify_tx_env(|env| *env = tx_env).build();
 
+                // Transact the transaction
+                let result = evm.transact().map_err(|err| TransactionError::TracingError(err.into()))?;
+
+                // Accumulate the traces
+                // TODO: it looks like for now the traces get accumulated in the context, which causes subsequent
+                // transactions to be added as subtraces of the previous ones. This is not the desired behavior.
                 let parity_builder = evm.context.external.clone().into_parity_builder();
                 let transaction_info = TransactionInfo {
                     hash: Some(tx.hash),
                     index: tx.transaction_index.map(|i| i.to()),
                     block_hash: tx.block_hash,
-                    block_number: tx.block_number.map(|i| i.to()),
+                    block_number: tx.block_number.map(|bn| bn.to()),
+                    // TODO: only for EIP1559 transactions?
                     base_fee: tx
                         .max_fee_per_gas
                         .map(|fee| (fee - tx.max_priority_fee_per_gas.unwrap_or_default()).to()),
                 };
                 traces.extend(parity_builder.into_localized_transaction_traces(transaction_info));
+
+                // Only commit to the database if there are more transactions to process.
+                if transactions.peek().is_some() {
+                    evm.context.evm.inner.db.commit(result.state);
+                }
             }
 
             Result::<_, EthApiError>::Ok(traces)
