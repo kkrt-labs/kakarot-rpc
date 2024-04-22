995f474f6fec3221e9dcb673e7e744b5bc88dce8
Elias Tazartes <66871571+Eikix@users.noreply.github.com>
2024-04-22 11:35:09 +0200

    Update src/test_utils/constants.rs
    Co-authored-by: greged93 <82421016+greged93@users.noreply.github.com>

--- src/test_utils/constants.rs
+++ src/test_utils/constants.rs
@@ -3,7 +3,7 @@ pub const ACCOUNT_IMPLEMENTATION: &str = "Account_implementation";
 pub const ACCOUNT_NONCE: &str = "Account_nonce";
 pub const ACCOUNT_STORAGE: &str = "Account_storage";
 pub const OWNABLE_OWNER: &str = "Ownable_owner";
-pub const ACCOUNT_CAIRO1_HELPERS_CLASS: &str = "Account_cairo1_helpers_class_hash";
+pub const ACCOUNT_CAIRO1_HELPERS_CLASS_HASH: &str = "Account_cairo1_helpers_class_hash";
 
 pub const KAKAROT_EVM_TO_STARKNET_ADDRESS: &str = "Kakarot_evm_to_starknet_address";
 pub const KAKAROT_NATIVE_TOKEN_ADDRESS: &str = "Kakarot_native_token_address";
