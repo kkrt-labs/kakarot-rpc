a8108bbef0f966067cdf6c46a03cd39cc135265e
Thomas Coratger <thomas.coratger@gmail.com>
2024-04-26 18:16:53 +0200

    fix comments

--- src/eth_rpc/servers/alchemy_rpc.rs
+++ src/eth_rpc/servers/alchemy_rpc.rs
@@ -23,9 +23,10 @@ impl<P: EthereumProvider> AlchemyRpc<P> {
 impl<P: EthereumProvider + Send + Sync + 'static> AlchemyApiServer for AlchemyRpc<P> {
     #[tracing::instrument(skip_all, ret, fields(address = %address, token_addresses = ?token_addresses))]
     async fn token_balances(&self, address: Address, token_addresses: Vec<Address>) -> Result<TokenBalances> {
+        let block_id = BlockId::Number(BlockNumberOrTag::Latest);
         let handles = token_addresses.into_iter().map(|token_addr| {
             let token = EthereumErc20::new(token_addr, &self.eth_provider);
-            let balance = token.balance_of(address, Default::default());
+            let balance = token.balance_of(address, block_id);
 
             FutureTokenBalance::new(Box::pin(balance), token_addr)
         });
