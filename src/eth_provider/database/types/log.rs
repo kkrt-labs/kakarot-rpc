3ca3165f1541decd7a734ae0d12c3fb9952ee4ca
Gregory Edison <gregory.edison1993@gmail.com>
2024-05-03 13:41:31 +0200

    add test

--- src/eth_provider/database/types/log.rs
+++ src/eth_provider/database/types/log.rs
@@ -1,8 +1,10 @@
 use reth_rpc_types::Log;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+
+use super::receipt::StoredTransactionReceipt;
 
 /// A transaction receipt as stored in the database
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone, PartialEq, Eq, Serialize)]
 pub struct StoredLog {
     #[serde(deserialize_with = "crate::eth_provider::database::types::serde::deserialize_intermediate")]
     pub log: Log,
@@ -11,5 +13,17 @@ pub struct StoredLog {
 impl From<StoredLog> for Log {
     fn from(log: StoredLog) -> Self {
         log.log
+    }
+}
+
+impl From<Log> for StoredLog {
+    fn from(log: Log) -> Self {
+        StoredLog { log }
+    }
+}
+
+impl From<StoredTransactionReceipt> for Vec<StoredLog> {
+    fn from(value: StoredTransactionReceipt) -> Self {
+        value.receipt.inner.logs().iter().cloned().map(Into::<StoredLog>::into).collect()
     }
 }
