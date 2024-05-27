ee5d6e9ac9c34a8d6351c4dff683aa1084106be3
Gregory Edison <gregory.edison1993@gmail.com>
2024-05-27 08:54:01 +0200

    answer comments

--- indexer/src/utils/filter.ts
+++ indexer/src/utils/filter.ts
@@ -15,21 +15,19 @@ export const isKakarotTransaction = (transaction: Transaction) => {
   // dataLength <- calldata[4]
   // calldataLen <- calldata[5]
   const calldata = transaction.invokeV1?.calldata;
-  if (calldata === undefined) {
+  if (!calldata) {
     console.error("No calldata in transaction");
     console.error(JSON.stringify(transaction, null, 2));
     return false;
   }
   const to = calldata[1];
-  if (to === undefined) {
+  if (!to) {
     console.error("No `to` field in calldata of transaction");
     console.error(JSON.stringify(transaction, null, 2));
     return false;
   }
-  // TODO(Greged93): replace this with a more robust check.
-  // ⚠️ The existence of `to` field in invoke calldata in RPC is not enforced by protocol.
-  // Forks or modifications of the kkrt-labs/kakarot-rpc codebase could break this check.
-  if (BigInt(to) !== BigInt(KAKAROT_ADDRESS!)) {
+
+  if (BigInt(to) !== BigInt(KAKAROT_ADDRESS)) {
     console.log("✅ Skipping transaction that is not related to Kakarot");
     return false;
   }
@@ -37,25 +35,26 @@ export const isKakarotTransaction = (transaction: Transaction) => {
 };
 
 export const ethValidationFailed = (event: Event) => {
-  if (event.data.length === 0) {
+  const { data } = event;
+  if (data.length === 0) {
     return false;
   }
 
-  const response_len = parseInt(event.data[0], 16);
+  const response_len = parseInt(data[0], 16);
 
-  if (response_len + 1 >= event.data.length) {
+  if (response_len + 1 >= data.length) {
     console.error(
-      `Invalid event data length. Got ${event.data.length}, expected < ${response_len + 1}`,
+      `Invalid event data length. Got ${data.length}, expected < ${response_len + 1}`,
     );
     return false;
   }
 
-  const success = parseInt(event.data[1 + response_len], 16);
+  const success = parseInt(data[1 + response_len], 16);
   if (success == 1) {
     return false;
   }
 
-  const response = event.data.slice(1, 1 + response_len);
+  const response = data.slice(1, 1 + response_len);
   const msg = String.fromCharCode(...response.map((x) => parseInt(x, 16)));
 
   return msg.includes("eth validation failed");
