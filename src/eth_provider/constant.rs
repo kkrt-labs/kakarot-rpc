2db566bc8f167bc8d278310234029be6a5d2e587
Joaquin Barrientos <joaquin2899@gmail.com>
2024-06-08 08:18:18 -0600

    dev: move TRANSACTION_MAX_RETRIES to env

--- src/eth_provider/constant.rs
+++ src/eth_provider/constant.rs
@@ -20,7 +20,10 @@ pub const ADDRESS_HEX_STRING_LEN: usize = 40;
 /// Starknet Modulus: 0x800000000000011000000000000000000000000000000000000000000000001
 pub const STARKNET_MODULUS: U256 = U256::from_limbs([0x1, 0, 0, 0x0800_0000_0000_0011]);
 /// Maximum number of times a transaction can be retried
-pub const TRANSACTION_MAX_RETRIES: u8 = 10;
+const TRANSACTION_MAX_RETRIES: u8 = u8::from_str(
+    &std::env::var("TRANSACTION_MAX_RETRIES")
+        .unwrap_or_else(|_| panic!("Missing environment variable TRANSACTION_MAX_RETRIES"))
+).expect("failing to parse TRANSACTION_MAX_RETRIES");
 
 #[cfg(feature = "hive")]
 use {
