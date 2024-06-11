1e5b7bb34d55eb4acd4a47357e1f74e7710f9733
greged93 <82421016+greged93@users.noreply.github.com>
2024-06-11 15:57:44 +0200

    Dev/staging (#1167)
    * staging ci
    
    * fix
    
    * change to string
    
    * run
    
    * run
    
    * fix
    
    * fix
    
    * run
    
    * fix
    
    * bump kakarot
    
    * update submodule
    
    * point submodule to fix branch
    
    * add kakarot fixes
    
    * test diff
    
    * test PR flow
    
    * fix PR staging
    
    * fix staging PR
    
    * fix staging workflow
    
    * conclude staging
    
    * fix comments

---
+++ scripts/staging.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd ../lib/kakarot || exit
+poetry run python ./kakarot_scripts/deploy_kakarot.py
+
+KAKAROT_ADDRESS=$(jq -r '.kakarot.address' ./deployments/kakarot-staging/deployments.json)
+UNINITIALIZED_ACCOUNT_CLASS_HASH=$(jq -r '.uninitialized_account' ./deployments/kakarot-staging/declarations.json)
+ACCOUNT_CONTRACT_CLASS_HASH=$(jq -r '.account_contract' ./deployments/kakarot-staging/declarations.json)
+
+export KAKAROT_ADDRESS="${KAKAROT_ADDRESS}"
+export UNINITIALIZED_ACCOUNT_CLASS_HASH="${UNINITIALIZED_ACCOUNT_CLASS_HASH}"
+export ACCOUNT_CONTRACT_CLASS_HASH="${ACCOUNT_CONTRACT_CLASS_HASH}"
+
+poetry run pytest -s tests/end_to_end -m 'not CairoPrecompiles'
