51c79d3f999f08caaf3900b558d19db98819dc91
Eugenio Paluello <eugypalu@gmail.com>
2024-06-25 08:38:13 +0200

    fix: calculate coverage drop against main branch

--- indexer/src/utils/filter.test.ts
+++ indexer/src/utils/filter.test.ts
@@ -18,237 +18,237 @@ const event = (data: `0x${string}`[]) => {
   } as Event;
 };
 
-// Deno.test("ethValidationFailed: failed transaction", () => {
-//   const x = event([
-//     "0x1e",
-//     "0x4b",
-//     "0x61",
-//     "0x6b",
-//     "0x61",
-//     "0x72",
-//     "0x6f",
-//     "0x74",
-//     "0x3a",
-//     "0x20",
-//     "0x65",
-//     "0x74",
-//     "0x68",
-//     "0x20",
-//     "0x76",
-//     "0x61",
-//     "0x6c",
-//     "0x69",
-//     "0x64",
-//     "0x61",
-//     "0x74",
-//     "0x69",
-//     "0x6f",
-//     "0x6e",
-//     "0x20",
-//     "0x66",
-//     "0x61",
-//     "0x69",
-//     "0x6c",
-//     "0x65",
-//     "0x64",
-//     "0x0",
-//     "0x0",
-//   ]);
-//   assert(ethValidationFailed(x));
-// });
+Deno.test("ethValidationFailed: failed transaction", () => {
+  const x = event([
+    "0x1e",
+    "0x4b",
+    "0x61",
+    "0x6b",
+    "0x61",
+    "0x72",
+    "0x6f",
+    "0x74",
+    "0x3a",
+    "0x20",
+    "0x65",
+    "0x74",
+    "0x68",
+    "0x20",
+    "0x76",
+    "0x61",
+    "0x6c",
+    "0x69",
+    "0x64",
+    "0x61",
+    "0x74",
+    "0x69",
+    "0x6f",
+    "0x6e",
+    "0x20",
+    "0x66",
+    "0x61",
+    "0x69",
+    "0x6c",
+    "0x65",
+    "0x64",
+    "0x0",
+    "0x0",
+  ]);
+  assert(ethValidationFailed(x));
+});
 
-// Deno.test(
-//   "ethValidationFailed: wrong failure message should not be taken into account",
-//   () => {
-//     const x = event([
-//       "0x1e",
-//       "0x4b",
-//       "0x61",
-//       "0x6b",
-//       "0x61",
-//       "0x72",
-//       "0x6f",
-//       "0x74",
-//       "0x3a",
-//       "0x20",
-//       "0x65",
-//       "0x74",
-//       "0x68",
-//       "0x20",
-//       "0x76",
-//       "0x61",
-//       "0x6c",
-//       "0x69",
-//       "0x64",
-//       "0x61",
-//       "0x74",
-//       "0x69",
-//       "0x6f",
-//       "0x65", // Modify this randomly to change the output message
-//       "0x20",
-//       "0x66",
-//       "0x61",
-//       "0x69",
-//       "0x6c",
-//       "0x65",
-//       "0x64",
-//       "0x0",
-//       "0x0",
-//     ]);
-//     assertFalse(ethValidationFailed(x));
-//   },
-// );
+Deno.test(
+  "ethValidationFailed: wrong failure message should not be taken into account",
+  () => {
+    const x = event([
+      "0x1e",
+      "0x4b",
+      "0x61",
+      "0x6b",
+      "0x61",
+      "0x72",
+      "0x6f",
+      "0x74",
+      "0x3a",
+      "0x20",
+      "0x65",
+      "0x74",
+      "0x68",
+      "0x20",
+      "0x76",
+      "0x61",
+      "0x6c",
+      "0x69",
+      "0x64",
+      "0x61",
+      "0x74",
+      "0x69",
+      "0x6f",
+      "0x65", // Modify this randomly to change the output message
+      "0x20",
+      "0x66",
+      "0x61",
+      "0x69",
+      "0x6c",
+      "0x65",
+      "0x64",
+      "0x0",
+      "0x0",
+    ]);
+    assertFalse(ethValidationFailed(x));
+  },
+);
 
-// Deno.test("ethValidationFailed: empty data", () => {
-//   const x = event([]);
-//   assertFalse(ethValidationFailed(x));
-// });
+Deno.test("ethValidationFailed: empty data", () => {
+  const x = event([]);
+  assertFalse(ethValidationFailed(x));
+});
 
-// Deno.test("ethValidationFailed: success true", () => {
-//   const x = event(["0x1", "0x1", "0x1", "0x1"]);
-//   assertFalse(ethValidationFailed(x));
-// });
+Deno.test("ethValidationFailed: success true", () => {
+  const x = event(["0x1", "0x1", "0x1", "0x1"]);
+  assertFalse(ethValidationFailed(x));
+});
 
-// Deno.test("ethValidationFailed: incorrect data length", () => {
-//   const x = event(["0x10"]);
-//   assertFalse(ethValidationFailed(x));
-// });
+Deno.test("ethValidationFailed: incorrect data length", () => {
+  const x = event(["0x10"]);
+  assertFalse(ethValidationFailed(x));
+});
 
-// Deno.test(
-//   "isKakarotTransaction: InvokeTransactionV0 should not be indexed",
-//   () => {
-//     const starknetTxCalldata: `0x${string}`[] = ["0x1", "0x1"];
-//     const transaction: Transaction = {
-//       invokeV0: {
-//         contractAddress: "0x01",
-//         entryPointSelector: "0x01",
-//         calldata: starknetTxCalldata,
-//       },
-//       meta: {
-//         hash: "0x01",
-//         maxFee: "0x01",
-//         nonce: "0x01",
-//         signature: ["0x1", "0x2", "0x3", "0x4", "0x32"],
-//         version: "1",
-//       },
-//     };
-//     assertFalse(isKakarotTransaction(transaction));
-//   },
-// );
+Deno.test(
+  "isKakarotTransaction: InvokeTransactionV0 should not be indexed",
+  () => {
+    const starknetTxCalldata: `0x${string}`[] = ["0x1", "0x1"];
+    const transaction: Transaction = {
+      invokeV0: {
+        contractAddress: "0x01",
+        entryPointSelector: "0x01",
+        calldata: starknetTxCalldata,
+      },
+      meta: {
+        hash: "0x01",
+        maxFee: "0x01",
+        nonce: "0x01",
+        signature: ["0x1", "0x2", "0x3", "0x4", "0x32"],
+        version: "1",
+      },
+    };
+    assertFalse(isKakarotTransaction(transaction));
+  },
+);
 
-// Deno.test(
-//   "isKakarotTransaction: L1HandlerTransaction should not be indexed",
-//   () => {
-//     const starknetTxCalldata: `0x${string}`[] = ["0x1", "0x1"];
-//     const transaction: Transaction = {
-//       l1Handler: {
-//         contractAddress: "0x01",
-//         entryPointSelector: "0x01",
-//         calldata: starknetTxCalldata,
-//       },
-//       meta: {
-//         hash: "0x01",
-//         maxFee: "0x01",
-//         nonce: "0x01",
-//         signature: ["0x1", "0x2", "0x3", "0x4", "0x32"],
-//         version: "1",
-//       },
-//     };
-//     assertFalse(isKakarotTransaction(transaction));
-//   },
-// );
+Deno.test(
+  "isKakarotTransaction: L1HandlerTransaction should not be indexed",
+  () => {
+    const starknetTxCalldata: `0x${string}`[] = ["0x1", "0x1"];
+    const transaction: Transaction = {
+      l1Handler: {
+        contractAddress: "0x01",
+        entryPointSelector: "0x01",
+        calldata: starknetTxCalldata,
+      },
+      meta: {
+        hash: "0x01",
+        maxFee: "0x01",
+        nonce: "0x01",
+        signature: ["0x1", "0x2", "0x3", "0x4", "0x32"],
+        version: "1",
+      },
+    };
+    assertFalse(isKakarotTransaction(transaction));
+  },
+);
 
-// Deno.test("isKakarotTransaction: no calldata", () => {
-//   const transaction: Transaction = {
-//     invokeV1: {
-//       senderAddress: "0x01",
-//       calldata: [],
-//     },
-//     meta: {
-//       hash: "0x01",
-//       maxFee: "0x01",
-//       nonce: "0x01",
-//       signature: ["0x1", "0x2", "0x3", "0x4", "0x32"],
-//       version: "1",
-//     },
-//   };
-//   assertFalse(isKakarotTransaction(transaction));
-// });
+Deno.test("isKakarotTransaction: no calldata", () => {
+  const transaction: Transaction = {
+    invokeV1: {
+      senderAddress: "0x01",
+      calldata: [],
+    },
+    meta: {
+      hash: "0x01",
+      maxFee: "0x01",
+      nonce: "0x01",
+      signature: ["0x1", "0x2", "0x3", "0x4", "0x32"],
+      version: "1",
+    },
+  };
+  assertFalse(isKakarotTransaction(transaction));
+});
 
-// Deno.test("isKakarotTransaction: no `to` field in calldata", () => {
-//   const starknetTxCalldata: `0x${string}`[] = ["0x1"];
-//   const transaction: Transaction = {
-//     invokeV1: {
-//       senderAddress: "0x01",
-//       calldata: starknetTxCalldata,
-//     },
-//     meta: {
-//       hash: "0x01",
-//       maxFee: "0x01",
-//       nonce: "0x01",
-//       signature: ["0x1", "0x2", "0x3", "0x4", "0x32"],
-//       version: "1",
-//     },
-//   };
-//   assertFalse(isKakarotTransaction(transaction));
-// });
+Deno.test("isKakarotTransaction: no `to` field in calldata", () => {
+  const starknetTxCalldata: `0x${string}`[] = ["0x1"];
+  const transaction: Transaction = {
+    invokeV1: {
+      senderAddress: "0x01",
+      calldata: starknetTxCalldata,
+    },
+    meta: {
+      hash: "0x01",
+      maxFee: "0x01",
+      nonce: "0x01",
+      signature: ["0x1", "0x2", "0x3", "0x4", "0x32"],
+      version: "1",
+    },
+  };
+  assertFalse(isKakarotTransaction(transaction));
+});
 
-// Deno.test(
-//   "isKakarotTransaction: `to` address not matching KAKAROT_ADDRESS",
-//   () => {
-//     const starknetTxCalldata: `0x${string}`[] = ["0x1", "0x2"];
-//     const transaction: Transaction = {
-//       invokeV1: {
-//         senderAddress: "0x01",
-//         calldata: starknetTxCalldata,
-//       },
-//       meta: {
-//         hash: "0x02",
-//         maxFee: "0x02",
-//         nonce: "0x02",
-//         signature: ["0x2"],
-//         version: "1",
-//       },
-//     };
-//     assertFalse(isKakarotTransaction(transaction));
-//   },
-// );
+Deno.test(
+  "isKakarotTransaction: `to` address not matching KAKAROT_ADDRESS",
+  () => {
+    const starknetTxCalldata: `0x${string}`[] = ["0x1", "0x2"];
+    const transaction: Transaction = {
+      invokeV1: {
+        senderAddress: "0x01",
+        calldata: starknetTxCalldata,
+      },
+      meta: {
+        hash: "0x02",
+        maxFee: "0x02",
+        nonce: "0x02",
+        signature: ["0x2"],
+        version: "1",
+      },
+    };
+    assertFalse(isKakarotTransaction(transaction));
+  },
+);
 
-// Deno.test("isKakarotTransaction: `to` address matching KAKAROT_ADDRESS", () => {
-//   const starknetTxCalldata: `0x${string}`[] = ["0x1", "0x1"];
-//   const transaction: Transaction = {
-//     invokeV1: {
-//       senderAddress: "0x01",
-//       calldata: starknetTxCalldata,
-//     },
-//     meta: {
-//       hash: "0x01",
-//       maxFee: "0x01",
-//       nonce: "0x01",
-//       signature: ["0x1", "0x2", "0x3", "0x4", "0x1"],
-//       version: "1",
-//     },
-//   };
-//   assert(isKakarotTransaction(transaction));
-// });
+Deno.test("isKakarotTransaction: `to` address matching KAKAROT_ADDRESS", () => {
+  const starknetTxCalldata: `0x${string}`[] = ["0x1", "0x1"];
+  const transaction: Transaction = {
+    invokeV1: {
+      senderAddress: "0x01",
+      calldata: starknetTxCalldata,
+    },
+    meta: {
+      hash: "0x01",
+      maxFee: "0x01",
+      nonce: "0x01",
+      signature: ["0x1", "0x2", "0x3", "0x4", "0x1"],
+      version: "1",
+    },
+  };
+  assert(isKakarotTransaction(transaction));
+});
 
-// Deno.test(
-//   "isRevertedWithOutOfResources: true on status reverted and revert reason",
-//   () => {
-//     const receipt: TransactionReceipt = {
-//       executionStatus: "EXECUTION_STATUS_REVERTED",
-//       transactionHash: "0x01",
-//       transactionIndex: "0x01",
-//       actualFee: "0x01",
-//       contractAddress: "0x01",
-//       l2ToL1Messages: [],
-//       events: [],
-//       revertReason:
-//         "Could not reach the end of the program. RunResources has no remaining steps",
-//     };
-//     assert(isRevertedWithOutOfResources(receipt));
-//   },
-// );
+Deno.test(
+  "isRevertedWithOutOfResources: true on status reverted and revert reason",
+  () => {
+    const receipt: TransactionReceipt = {
+      executionStatus: "EXECUTION_STATUS_REVERTED",
+      transactionHash: "0x01",
+      transactionIndex: "0x01",
+      actualFee: "0x01",
+      contractAddress: "0x01",
+      l2ToL1Messages: [],
+      events: [],
+      revertReason:
+        "Could not reach the end of the program. RunResources has no remaining steps",
+    };
+    assert(isRevertedWithOutOfResources(receipt));
+  },
+);
 
 Deno.test(
   "isRevertedWithOutOfResources: false on status reverted and no revert reason",
--- indexer/src/utils/hex.test.ts
+++ indexer/src/utils/hex.test.ts
@@ -1,135 +1,135 @@
 import { assertEquals } from "https://deno.land/std@0.213.0/assert/mod.ts";
 import { padBigint, padBytes, padString, toHexString } from "./hex.ts";
 
-// Deno.test("toHexString converts string to hex format", () => {
-//   const x = "1234";
-//   const expected = "0x4d2";
-//   const paddedX = toHexString(x);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("toHexString converts string to hex format", () => {
+  const x = "1234";
+  const expected = "0x4d2";
+  const paddedX = toHexString(x);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("toHexString handles undefined input", () => {
-//   const x = undefined;
-//   const expected = "0x";
-//   const paddedX = toHexString(x);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("toHexString handles undefined input", () => {
+  const x = undefined;
+  const expected = "0x";
+  const paddedX = toHexString(x);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padString pads hex string to specified length", () => {
-//   const x = "0x010203";
-//   const expected = "0x0000000000010203";
-//   const paddedX = padString(x, 8);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padString pads hex string to specified length", () => {
+  const x = "0x010203";
+  const expected = "0x0000000000010203";
+  const paddedX = padString(x, 8);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padString does not pad when hex string length is equal to specified length", () => {
-//   const x = "0x010203";
-//   const expected = "0x010203";
-//   const paddedX = padString(x, 3);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padString does not pad when hex string length is equal to specified length", () => {
+  const x = "0x010203";
+  const expected = "0x010203";
+  const paddedX = padString(x, 3);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padString handles hex string with length equal to specified length", () => {
-//   const x = "0x010203";
-//   const expected = "0x010203";
-//   const paddedX = padString(x, 2);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padString handles hex string with length equal to specified length", () => {
+  const x = "0x010203";
+  const expected = "0x010203";
+  const paddedX = padString(x, 2);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padString pads hex string to specified length", () => {
-//   const x = "0x0000000000010203";
-//   const expected = "0x0000000000010203";
-//   const paddedX = padString(x, 4);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padString pads hex string to specified length", () => {
+  const x = "0x0000000000010203";
+  const expected = "0x0000000000010203";
+  const paddedX = padString(x, 4);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padString handles undefined input", () => {
-//   const x = undefined;
-//   const expected = "0x0000000000000000";
-//   const paddedX = padString(x, 8);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padString handles undefined input", () => {
+  const x = undefined;
+  const expected = "0x0000000000000000";
+  const paddedX = padString(x, 8);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBigint pads bigint to specified length", () => {
-//   const x = BigInt("0x010203");
-//   const expected = "0x0000000000010203";
-//   const paddedX = padBigint(x, 8);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBigint pads bigint to specified length", () => {
+  const x = BigInt("0x010203");
+  const expected = "0x0000000000010203";
+  const paddedX = padBigint(x, 8);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBigint handles undefined input", () => {
-//   const x = undefined;
-//   const expected = "0x0000000000000000";
-//   const paddedX = padBigint(x, 8);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBigint handles undefined input", () => {
+  const x = undefined;
+  const expected = "0x0000000000000000";
+  const paddedX = padBigint(x, 8);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBigint with zero bigint to specified length", () => {
-//   const x = BigInt("0x0");
-//   const expected = "0x0000000000000000";
-//   const paddedX = padBigint(x, 8);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBigint with zero bigint to specified length", () => {
+  const x = BigInt("0x0");
+  const expected = "0x0000000000000000";
+  const paddedX = padBigint(x, 8);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBigint does not pad if hex length >= specified length", () => {
-//   const x = BigInt("0x010203040506070809");
-//   const expected = "0x10203040506070809";
-//   const paddedX = padBigint(x, 4);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBigint does not pad if hex length >= specified length", () => {
+  const x = BigInt("0x010203040506070809");
+  const expected = "0x10203040506070809";
+  const paddedX = padBigint(x, 4);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBigint handles smaller bigint and shorter padding length", () => {
-//   const x = BigInt("0x000000000000009");
-//   const expected = "0x00000009";
-//   const paddedX = padBigint(x, 4);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBigint handles smaller bigint and shorter padding length", () => {
+  const x = BigInt("0x000000000000009");
+  const expected = "0x00000009";
+  const paddedX = padBigint(x, 4);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBigint does not pad when length is equal", () => {
-//   const x = BigInt("0x0000000000000009");
-//   const expected = "0x0000000000000009";
-//   const paddedX = padBigint(x, 8);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBigint does not pad when length is equal", () => {
+  const x = BigInt("0x0000000000000009");
+  const expected = "0x0000000000000009";
+  const paddedX = padBigint(x, 8);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBytes pads Uint8Array to specified length", () => {
-//   const x = new Uint8Array([1, 2, 3]);
-//   const expected = "0x0000000000010203";
-//   const paddedX = padBytes(x, 8);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBytes pads Uint8Array to specified length", () => {
+  const x = new Uint8Array([1, 2, 3]);
+  const expected = "0x0000000000010203";
+  const paddedX = padBytes(x, 8);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBytes pads Uint8Array with larger data to specified length", () => {
-//   const x = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-//   const expected = "0x010203040506070809";
-//   const paddedX = padBytes(x, 8);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBytes pads Uint8Array with larger data to specified length", () => {
+  const x = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const expected = "0x010203040506070809";
+  const paddedX = padBytes(x, 8);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBytes pads Uint8Array with larger data to specified length (and zeros", () => {
-//   const x = new Uint8Array([0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-//   const expected = "0x0000010203040506070809";
-//   const paddedX = padBytes(x, 8);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBytes pads Uint8Array with larger data to specified length (and zeros", () => {
+  const x = new Uint8Array([0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const expected = "0x0000010203040506070809";
+  const paddedX = padBytes(x, 8);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBytes pads Uint8Array to exact specified length", () => {
-//   const x = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-//   const expected = "0x010203040506070809";
-//   const paddedX = padBytes(x, 9);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBytes pads Uint8Array to exact specified length", () => {
+  const x = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const expected = "0x010203040506070809";
+  const paddedX = padBytes(x, 9);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBytes pads Uint8Array empty to specified length", () => {
-//   const x = new Uint8Array([]);
-//   const expected = "0x000000000000000000";
-//   const paddedX = padBytes(x, 9);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBytes pads Uint8Array empty to specified length", () => {
+  const x = new Uint8Array([]);
+  const expected = "0x000000000000000000";
+  const paddedX = padBytes(x, 9);
+  assertEquals(paddedX, expected);
+});
 
-// Deno.test("padBytes handles undefined input", () => {
-//   const x = undefined;
-//   const expected = "0x0000000000000000";
-//   const paddedX = padBytes(x, 8);
-//   assertEquals(paddedX, expected);
-// });
+Deno.test("padBytes handles undefined input", () => {
+  const x = undefined;
+  const expected = "0x0000000000000000";
+  const paddedX = padBytes(x, 8);
+  assertEquals(paddedX, expected);
+});
