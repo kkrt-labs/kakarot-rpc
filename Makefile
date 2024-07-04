465b6600856ed994816cf51d980a5fd99fc5d3cd
Thomas Coratger <thomas.coratger@gmail.com>
2024-07-04 19:02:11 +0200

    fix comments

--- Makefile
+++ Makefile
@@ -59,9 +59,11 @@ test: katana-genesis load-env
 test-target: load-env
 	cargo test --tests --features testing $(TARGET) -- --nocapture
 
+test-target1: load-env
+	cargo test --package kakarot-rpc --test entry --all-features -- tests::alchemy_api::test_token_allowance --exact --show-output
+
 benchmark:
 	cd benchmarks && bun i && bun run benchmark
-
 
 docker-build: setup
 	docker build -t kakarot-rpc . -f docker/rpc/Dockerfile
--- src/eth_rpc/servers/alchemy_rpc.rs
+++ src/eth_rpc/servers/alchemy_rpc.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::blocks_in_conditions)]
+
 use futures::future::join_all;
 use jsonrpsee::core::{async_trait, RpcResult};
 use reth_primitives::{Address, BlockId, BlockNumberOrTag, U256};
@@ -25,7 +27,7 @@ impl<P: EthereumProvider> AlchemyRpc<P> {
 #[async_trait]
 impl<P: EthereumProvider + Send + Sync + 'static> AlchemyApiServer for AlchemyRpc<P> {
     /// Retrieves the token balances for a given address.
-    #[tracing::instrument(skip_all, ret, fields(address = %address, token_addresses = ?token_addresses))]
+    #[tracing::instrument(skip_all, ret, fields(address = %address), err)]
     async fn token_balances(&self, address: Address, token_addresses: Vec<Address>) -> RpcResult<TokenBalances> {
         tracing::info!("Serving alchemy_getTokenBalances");
 
@@ -48,7 +50,7 @@ impl<P: EthereumProvider + Send + Sync + 'static> AlchemyApiServer for AlchemyRp
     }
 
     /// Retrieves the metadata for a given token.
-    #[tracing::instrument(skip_all, ret, fields(token_address = %token_address))]
+    #[tracing::instrument(skip(self), ret, err)]
     async fn token_metadata(&self, token_address: Address) -> RpcResult<TokenMetadata> {
         tracing::info!("Serving alchemy_getTokenMetadata");
 
@@ -66,7 +68,7 @@ impl<P: EthereumProvider + Send + Sync + 'static> AlchemyApiServer for AlchemyRp
     }
 
     /// Retrieves the allowance of a given owner for a spender.
-    #[tracing::instrument(skip_all, ret, fields(token_address = %token_address, owner = %owner, spender = %spender))]
+    #[tracing::instrument(skip(self), ret, err)]
     async fn token_allowance(&self, token_address: Address, owner: Address, spender: Address) -> RpcResult<U256> {
         tracing::info!("Serving alchemy_getTokenAllowance");
 
