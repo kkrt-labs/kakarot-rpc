d35e9dee8c9a181244357cc92393ceb910902e97
Gregory Edison <gregory.edison1993@gmail.com>
2024-07-15 11:09:35 +0200

    update upgrade.sh

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,14 +2,14 @@
 
 # Function to display usage
 usage() {
-    echo "Usage: $0 [COMMAND] [OPTION]"
-    echo "Commands:"
-    echo "  test         Run tests"
-    echo "  deploy       Run deployments"
-    echo "Options:"
-    echo "  --staging    Use staging environment for command"
-    echo "  --production Use production environment for command"
-    exit 1
+	echo "Usage: $0 [COMMAND] [OPTION]"
+	echo "Commands:"
+	echo "  test         Run tests"
+	echo "  deploy       Run deployments"
+	echo "Options:"
+	echo "  --staging    Use staging environment for command"
+	echo "  --production Use production environment for command"
+	exit 1
 }
 
 # Check if at least one argument is passed
@@ -59,27 +59,25 @@ export STARKNET_NETWORK="kakarot-${ENV}"
 if [ "${ENV}" = "staging" ]; then
 	export EVM_PRIVATE_KEY="0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
 	export KAKAROT_STAGING_RPC_URL="https://juno-kakarot-testnet-stage.karnot.xyz"
+	export KAKAROT_STAGING_ACCOUNT_ADDRESS="0x7ecf6cd45c32ce84812e660cc176cb8b4de2e7a6d5916fe326bf871466fbe02"
 	export WEB3_HTTP_PROVIDER_URI="https://kkrt-rpc-kakarot-testnet-stage.karnot.xyz"
 	export KAKAROT_STAGING_ACCOUNT_ADDRESS="0x7ecf6cd45c32ce84812e660cc176cb8b4de2e7a6d5916fe326bf871466fbe02"
 	if [ -z "${KAKAROT_STAGING_PRIVATE_KEY}" ]; then
-		echo "Please provide the KAKAROT_STAGING_PRIVATE_KEY environment variable"
+		echo "Please provide the KAKAROT_STAGING_PRIVATE_KEY environment variable. The private key should be loaded using gpg: gpg -r recipient@kakarot.org --decrypt path/to/encripted/key.gpg"
 		exit 1
 	fi
 
 	SKIP="--ignore tests/end_to_end/L1L2Messaging"
 elif [ "${ENV}" = "sepolia" ]; then
 	export KAKAROT_SEPOLIA_RPC_URL="https://juno-kakarot-dev.karnot.xyz/"
+	export KAKAROT_SEPOLIA_ACCOUNT_ADDRESS="0x43ABAA073C768EBF039C0C4F46DB9ACC39E9EC165690418060A652AAB39E7D8"
 	export WEB3_HTTP_PROVIDER_URI="https://sepolia-rpc.kakarot.org"
 	if [ -z "${EVM_PRIVATE_KEY}" ]; then
 		echo "Please provide the EVM_PRIVATE_KEY environment variable"
 		exit 1
 	fi
-	if [ -z "${KAKAROT_SEPOLIA_ACCOUNT_ADDRESS}" ]; then
-		echo "Please provide the KAKAROT_SEPOLIA_ACCOUNT_ADDRESS environment variable"
-		exit 1
-	fi
 	if [ -z "${KAKAROT_SEPOLIA_PRIVATE_KEY}" ]; then
-		echo "Please provide the KAKAROT_SEPOLIA_PRIVATE_KEY environment variable"
+		echo "Please provide the KAKAROT_SEPOLIA_PRIVATE_KEY environment variable. The private key should be loaded using gpg: gpg -r recipient@kakarot.org --decrypt path/to/encripted/key.gpg"
 		exit 1
 	fi
 
