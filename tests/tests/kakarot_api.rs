24bb4305faa8ece73d4a0679e1f93a526affa152
Eugenio Paluello <eugypalu@gmail.com>
2024-07-23 15:12:08 +0200

    fix: test

--- tests/tests/kakarot_api.rs
+++ tests/tests/kakarot_api.rs
@@ -1,14 +1,9 @@
 #![allow(clippy::used_underscore_binding)]
 #![cfg(feature = "testing")]
 use kakarot_rpc::{
-    config::KakarotRpcConfig,
     eth_provider::{
-        constant::{Constant, MAX_LOGS},
-        database::types::transaction::StoredPendingTransaction,
-        provider::EthereumProvider,
-        starknet::kakarot_core::{get_white_listed_eip_155_transaction_hashes, MAX_FELTS_IN_CALLDATA},
+        constant::Constant, database::types::transaction::StoredPendingTransaction, provider::EthereumProvider,
     },
-    retry::{get_retry_tx_interval, get_transaction_max_retries},
     test_utils::{
         eoa::Eoa,
         fixtures::{katana, setup},
@@ -19,7 +14,7 @@ use kakarot_rpc::{
 use reth_primitives::{sign_message, Address, Bytes, Transaction, TransactionSigned, TxEip1559, TxKind, B256, U256};
 use rstest::*;
 use serde_json::Value;
-
+use std::str::FromStr;
 #[rstest]
 #[awt]
 #[tokio::test(flavor = "multi_thread")]
@@ -138,22 +133,37 @@ async fn test_kakarot_get_starknet_transaction_hash_with_none_tx_hash(#[future] 
 #[awt]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_kakarot_get_config(#[future] katana: Katana, _setup: ()) {
-    std::env::set_var("STARKNET_NETWORK", "http://0.0.0.0:1010");
-    std::env::set_var(
-        "WHITE_LISTED_EIP_155_TRANSACTION_HASHES",
-        "0xe65425dacfc1423823cb4766aa0192ffde61eaa9bf81af9fe15149a89ef36c28",
-    );
-    let starknet_config = KakarotRpcConfig::from_env().expect("Failed to load Kakarot RPC config");
+    // Define variables
+    let starknet_network = "http://0.0.0.0:1010/";
+    let white_listed_eip_155_transaction_hashes = "0xe65425dacfc1423823cb4766aa0192ffde61eaa9bf81af9fe15149a89ef36c28";
+    let max_logs = 10000;
+    let retry_tx_interval = 10;
+    let transaction_max_retries = 10;
+    let max_felts_in_calldata = 22500;
+
+    // Set environment variables for the test
+    std::env::set_var("STARKNET_NETWORK", starknet_network);
+    std::env::set_var("WHITE_LISTED_EIP_155_TRANSACTION_HASHES", white_listed_eip_155_transaction_hashes);
+    std::env::set_var("MAX_LOGS", max_logs.to_string());
+    std::env::set_var("RETRY_TX_INTERVAL", retry_tx_interval.to_string());
+    std::env::set_var("TRANSACTION_MAX_RETRIES", transaction_max_retries.to_string());
+    std::env::set_var("MAX_FELTS_IN_CALLDATA", max_felts_in_calldata.to_string());
+
+    // Hardcoded expected values
     let expected_constant = Constant {
-        max_logs: *MAX_LOGS,
-        starknet_network: String::from(starknet_config.network_url),
-        retry_tx_interval: get_retry_tx_interval(),
-        transaction_max_retries: get_transaction_max_retries(),
-        max_felts_in_calldata: *MAX_FELTS_IN_CALLDATA,
-        white_listed_eip_155_transaction_hashes: get_white_listed_eip_155_transaction_hashes(),
+        max_logs: Some(max_logs),
+        starknet_network: (starknet_network).to_string(),
+        retry_tx_interval,
+        transaction_max_retries,
+        max_felts_in_calldata,
+        white_listed_eip_155_transaction_hashes: vec![B256::from_str(white_listed_eip_155_transaction_hashes).unwrap()],
     };
+
+    // Start the Kakarot RPC server
     let (server_addr, server_handle) =
         start_kakarot_rpc_server(&katana).await.expect("Error setting up Kakarot RPC server");
+
+    // Send the RPC request to get the configuration
     let reqwest_client = reqwest::Client::new();
     let res = reqwest_client
         .post(format!("http://localhost:{}", server_addr.port()))
@@ -162,10 +172,13 @@ async fn test_kakarot_get_config(#[future] katana: Katana, _setup: ()) {
         .send()
         .await
         .expect("kakarot_getConfig error");
+
+    // Deserialize the response
     let result_constant: Constant = serde_json::from_str(&res.text().await.expect("Failed to get response body"))
         .and_then(|raw: Value| serde_json::from_value(raw["result"].clone()))
         .expect("Failed to deserialize response body or convert result to Constant");
 
+    // Assert that the returned configuration matches the expected value
     assert_eq!(result_constant, expected_constant);
 
     drop(server_handle);
