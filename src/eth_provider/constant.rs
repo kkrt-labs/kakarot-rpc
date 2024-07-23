79551bcef208ea08630b6eef11e51f6563d1660d
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-07-23 23:30:57 +0200

    Update src/eth_provider/constant.rs

--- src/eth_provider/constant.rs
+++ src/eth_provider/constant.rs
@@ -61,13 +61,19 @@ lazy_static! {
     pub static ref DEPLOY_WALLET_NONCE: Arc<Mutex<Felt>> = Arc::new(Mutex::new(Felt::ZERO));
 }
 
+/// Struct used to return the constant values from the `kakarot_getConfig` endpoint
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
-// Struct used to return the constant values from the kakarot_getConfig endpoint
 pub struct Constant {
+    /// Maximum number of logs to output for `eth_getLogs` RPC Method
     pub max_logs: Option<u64>,
+    /// Name of the StarkNet network.
     pub starknet_network: String,
+    /// Interval in seconds to retry transactions.
     pub retry_tx_interval: u64,
+    /// Maximum number of retries for a transaction.
     pub transaction_max_retries: u8,
+    /// Maximum number of Felts in calldata.
     pub max_felts_in_calldata: usize,
+    /// List of whitelisted hashes allow to submit pre EIP-155 transactions.
     pub white_listed_eip_155_transaction_hashes: Vec<B256>,
 }
