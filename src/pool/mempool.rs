0bdb1129872b855a56b3dd04e81759158332f7f7
Gregory Edison <gregory.edison1993@gmail.com>
2024-09-26 18:54:51 +0200

    add pool maintenance task

--- src/pool/mempool.rs
+++ src/pool/mempool.rs
@@ -1,15 +1,21 @@
 #![allow(clippy::significant_drop_tightening)]
 
 use super::validate::KakarotTransactionValidator;
+use crate::providers::eth_provider::database::state::EthDatabase;
+use crate::providers::eth_provider::BlockProvider;
 use crate::{
     client::EthClient,
     into_via_try_wrapper,
     providers::eth_provider::{constant::RPC_CONFIG, starknet::relayer::LockedRelayer},
 };
 use futures::future::select_all;
-use reth_primitives::{IntoRecoveredTransaction, U256};
+use reth_chainspec::ChainSpec;
+use reth_execution_types::ChangedAccount;
+use reth_primitives::{Address, BlockNumberOrTag, IntoRecoveredTransaction, U256};
+use reth_revm::DatabaseRef;
 use reth_transaction_pool::{
-    blobstore::NoopBlobStore, CoinbaseTipOrdering, EthPooledTransaction, Pool, TransactionOrigin, TransactionPool,
+    blobstore::NoopBlobStore, BlockInfo, CanonicalStateUpdate, CoinbaseTipOrdering, EthPooledTransaction, Pool,
+    TransactionOrigin, TransactionPool, TransactionPoolExt,
 };
 use serde_json::Value;
 use starknet::{
@@ -17,7 +23,7 @@ use starknet::{
     providers::{jsonrpc::HttpTransport, JsonRpcClient},
 };
 use std::{collections::HashMap, fs::File, io::Read, str::FromStr, sync::Arc, time::Duration};
-use tokio::{runtime::Handle, sync::Mutex};
+use tokio::sync::Mutex;
 
 /// A type alias for the Kakarot Transaction Validator.
 /// Uses the Reth implementation [`TransactionValidationTaskExecutor`].
@@ -101,9 +107,9 @@ impl<SP: starknet::providers::Provider + Send + Sync + Clone + 'static> AccountM
     }
 
     /// Starts the account manager task that periodically checks account balances and processes transactions.
-    pub fn start(self, rt_handle: &Handle) {
+    pub fn start(self) {
         let this = Arc::new(self);
-        rt_handle.spawn(async move {
+        tokio::spawn(async move {
             loop {
                 // TODO: add a listener on the pool and only try to call [`best_transaction`]
                 // TODO: when we are sure there is a transaction in the pool. This avoids an
@@ -217,6 +223,125 @@ impl<SP: starknet::providers::Provider + Send + Sync + Clone + 'static> AccountM
             .await
             .map_err(Into::into)
     }
+}
+
+#[derive(Default)]
+struct LoadedAccounts {
+    /// All accounts that were loaded
+    accounts: Vec<ChangedAccount>,
+    /// All accounts that failed to load
+    failed_to_load: Vec<Address>,
+}
+
+/// Loads all accounts at the given state
+///
+/// Note: this expects _unique_ addresses
+fn load_accounts<SP, I>(client: &Arc<EthClient<SP>>, addresses: I) -> LoadedAccounts
+where
+    SP: starknet::providers::Provider + Send + Sync + Clone + 'static,
+    I: IntoIterator<Item = Address>,
+{
+    let addresses = addresses.into_iter();
+    let mut res = LoadedAccounts::default();
+
+    let db = EthDatabase::new(Arc::new(client.eth_provider()), BlockNumberOrTag::Latest.into());
+
+    for addr in addresses {
+        if let Ok(maybe_acc) = db.basic_ref(addr) {
+            let acc = maybe_acc.map_or_else(
+                || ChangedAccount::empty(addr),
+                |acc| ChangedAccount { address: addr, nonce: acc.nonce, balance: acc.balance },
+            );
+            res.accounts.push(acc);
+        } else {
+            // failed to load account.
+            res.failed_to_load.push(addr);
+        }
+    }
+    res
+}
+
+/// Maintains the transaction pool by periodically polling the database in order to
+/// fetch the latest block and mark the block's transactions as mined by the node.
+pub fn maintain_transaction_pool<SP>(eth_client: Arc<EthClient<SP>>)
+where
+    SP: starknet::providers::Provider + Send + Sync + Clone + 'static,
+{
+    tokio::spawn(async move {
+        let mut block_number = 0u64;
+        loop {
+            // Fetch the latest block number
+            let Ok(current_block_number) = eth_client.eth_provider().block_number().await else {
+                tracing::error!(target: "maintain_transaction_pool", "failed to fetch current block number");
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                continue;
+            };
+
+            if current_block_number.to::<u64>() > block_number {
+                // Fetch the block by number for the latest block
+                if let Ok(Some(latest_block)) =
+                    eth_client.eth_provider().block_by_number(BlockNumberOrTag::Latest, true).await
+                {
+                    let hash = latest_block.header.hash;
+
+                    // If we can convert the RPC block to a primitive block, we proceed
+                    if let Ok(latest_block) = TryInto::<reth_primitives::Block>::try_into(latest_block.inner) {
+                        let latest_header = latest_block.header.clone().seal(hash);
+
+                        // Update the block information in the pool
+                        let chain_spec =
+                            ChainSpec { chain: eth_client.eth_provider().chain_id.into(), ..Default::default() };
+                        let info = BlockInfo {
+                            block_gas_limit: latest_header.gas_limit,
+                            last_seen_block_hash: latest_header.hash(),
+                            last_seen_block_number: latest_header.number,
+                            pending_basefee: latest_header
+                                .next_block_base_fee(
+                                    chain_spec.base_fee_params_at_timestamp(latest_header.timestamp + 12),
+                                )
+                                .unwrap_or_default(),
+                            pending_blob_fee: latest_header.next_block_blob_fee(),
+                        };
+                        eth_client.mempool().set_block_info(info);
+
+                        // Fetch unique senders from the mempool that are out of sync
+                        let dirty_addresses = eth_client.mempool().unique_senders();
+
+                        let mut changed_accounts = Vec::new();
+
+                        // if we have accounts that are out of sync with the pool, we reload them in chunks
+                        if !dirty_addresses.is_empty() {
+                            // can fetch all dirty accounts at once
+                            let reloaded = load_accounts(&eth_client.clone(), dirty_addresses);
+                            changed_accounts.extend(reloaded.accounts);
+                            // update the pool with the loaded accounts
+                            eth_client.mempool().update_accounts(changed_accounts.clone());
+                        }
+
+                        let sealed_block = latest_block.seal(hash);
+                        let mined_transactions = sealed_block.body.iter().map(|tx| tx.hash).collect();
+
+                        // Canonical update
+                        let update = CanonicalStateUpdate {
+                            new_tip: &sealed_block,
+                            pending_block_base_fee: info.pending_basefee,
+                            pending_block_blob_fee: None,
+                            changed_accounts,
+                            mined_transactions,
+                        };
+                        eth_client.mempool().on_canonical_state_change(update);
+
+                        block_number = current_block_number.to();
+                    } else {
+                        tracing::error!(target: "maintain_transaction_pool", "failed to convert block");
+                    }
+                } else {
+                    tracing::error!(target: "maintain_transaction_pool", "failed to fetch latest block");
+                }
+            }
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
+    });
 }
 
 #[cfg(test)]
