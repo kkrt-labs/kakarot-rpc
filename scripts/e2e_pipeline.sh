01b426bb28a304f1a64358de75e48e6c255cee51
Elias Tazartes <elias.tazartes@gmail.com>
2024-11-07 11:42:15 +0100

    fix add relayer in e2e tests

--- scripts/e2e_pipeline.sh
+++ scripts/e2e_pipeline.sh
@@ -78,6 +78,14 @@ if [ "${ENV}" = "sepolia" ]; then
 		echo "Please provide the EVM_PRIVATE_KEY environment variable."
 		exit 1
 	fi
+	if [ -z "${STARKNET_SEPOLIA_RELAYER_ACCOUNT_ADDRESS}" ]; then
+		echo "Please provide the STARKNET_SEPOLIA_RELAYER_ACCOUNT_ADDRESS environment variable."
+		exit 1
+	fi
+	if [ -z "${STARKNET_SEPOLIA_RELAYER_PRIVATE_KEY}" ]; then
+		echo "Please provide the STARKNET_SEPOLIA_RELAYER_PRIVATE_KEY environment variable."
+		exit 1
+	fi
 	SKIP="--ignore tests/end_to_end/L1L2Messaging --ignore tests/end_to_end/CairoPrecompiles -k 'not test_should_set_account_bytecode and not test_should_set_account_nonce and not test_should_upgrade_account_class and not test_should_raise_when_class_hash_is_not_declared and not test_should_upgrade_class_hash and not test_should_transfer_ownership and not test_should_return_transaction_count and not test_should_withdraw_all_eth'"
 elif [ "${ENV}" = "sepolia-staging" ]; then
 	export EVM_PRIVATE_KEY="0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
@@ -92,6 +100,14 @@ elif [ "${ENV}" = "sepolia-staging" ]; then
 	fi
 	if [ -z "${STARKNET_SEPOLIA_STAGING_PRIVATE_KEY}" ]; then
 		echo "Please provide the STARKNET_SEPOLIA_STAGING_PRIVATE_KEY environment variable."
+		exit 1
+	fi
+	if [ -z "${STARKNET_SEPOLIA_STAGING_RELAYER_ACCOUNT_ADDRESS}" ]; then
+		echo "Please provide the STARKNET_SEPOLIA_STAGING_RELAYER_ACCOUNT_ADDRESS environment variable."
+		exit 1
+	fi
+	if [ -z "${STARKNET_SEPOLIA_STAGING_RELAYER_PRIVATE_KEY}" ]; then
+		echo "Please provide the STARKNET_SEPOLIA_STAGING_RELAYER_PRIVATE_KEY environment variable."
 		exit 1
 	fi
 
